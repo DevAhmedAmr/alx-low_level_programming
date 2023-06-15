ecb2fa4c5c19988fb2a3b363d16ac05246cfd6b7
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-15 10:11:54 -0700

    5. Hello, printf (automated command)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: application to print some text using printf function
+*
+* Return: 0 (success) else (failed)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
