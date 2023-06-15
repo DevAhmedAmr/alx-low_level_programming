7121aa8e9c8d1b803d1d065e22a6ab4ee0f91ce1
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-15 11:12:58 -0700

    test (automated command)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Description: app that prints some text , to the standard error.
+*
+*Returns: 0 (success) : 1 (failed)
+*/
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
