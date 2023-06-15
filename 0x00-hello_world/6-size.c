62748268007c81c5c4ad6db2cc09a9c4a8fcca87
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-15 10:29:51 -0700

    6. Size is not grandeur, and territory does not make a nation (automated command)

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/**
+ *
+ * main - Entry point
+ *
+ * Description: Application to print the size of various types on the computer it is compiled and run on.
+ *
+ * Return: 0 (success) else (failed)
+ */
+
+int main(void)
+{
+char character = 'a';
+int integer = 1;
+long int longInteger = 100000;
+long long int longLongInt = 1000000000000;
+float floatNum = 3.44;
+
+printf("Size of a char: %d byte(s)\n", sizeof(character));
+printf("Size of a int: %d byte(s)\n", sizeof(integer));
+printf("Size of a long int : %d byte(s)\n", sizeof(longInteger));
+printf("Size of a long long int : %d byte(s)\n", sizeof(longLongInt));
+printf("Size of a float : %d byte(s)\n", sizeof(floatNum));
+}
