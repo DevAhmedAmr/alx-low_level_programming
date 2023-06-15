cf510b388ff043d9e0aacc435492b0f61aeec7af
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-15 10:04:55 -0700

    4. Hello, puts (automated command)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A C program that prints some text with puts function
+*
+* Return: 0 (success) else (failed)
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-
-return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
