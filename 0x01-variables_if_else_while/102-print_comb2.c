3404f300bc9a214c25bfb52a2ba0199ec0bcf03b
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-16 14:11:25 -0700

    test (automated command)

---
+++ 0x01-variables_if_else_while/102-print_comb2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:a program that prints all possible combinations.
+ * of two two-digit numbers.
+ *
+ * Return: 0 success, else failure
+ */
+#include<stdio.h>
+
+int main(void)
+{
+int i=0;
+for(;i<=99;i++)
+{
+int j=i;
+for(;j<=99;j++)
+{
+if(i!=j)
+{
+putchar(i/10+'0');
+putchar(i%10+'0');
+putchar(' ');
+putchar(j/10+'0');
+putchar(j%10+'0');
+if(i!=99||j!=99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return(0);
+}
