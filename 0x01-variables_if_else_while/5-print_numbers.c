79345898934ebd2721242e3ffc39a3ab6e230727
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-16 07:23:10 -0700

    5. Numbers  (automated command)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: program that prints all single digit numbers of base 10
+* starting from 0, followed by a new line.
+*
+* Return: 0 succsess , else failed
+*/
+int main(void)
+{
+int i = 48;
+for (; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
