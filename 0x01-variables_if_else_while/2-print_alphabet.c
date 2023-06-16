c4dc7cc18d24371d5c5f5d4d6c2850650c4df236
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-16 06:40:25 -0700

    0. Positive anything is better than negative nothing  (automated command)

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: program that prints the alphabet in lowercase, followed by a new line.
+*
+* Return: 0 succsess , else failed
+*/
+
+int main(void)
+{
+for (size_t i = 97; i < 123; i++){
+putchar(i);
+}
+putchar(10);
+return (0);
+}
