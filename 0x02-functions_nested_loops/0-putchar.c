d87a650071e63a4aabc9745a32c02dbb9b748405
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-20 08:37:12 -0700

    0. _putchar (automated command)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints _putchar, followed by a new line.
+ *
+ * Return: 0 succsess , else failed
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
