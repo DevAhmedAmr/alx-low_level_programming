f4d6ab072bdb723cbf89baaa4d18043e66f3cb5d
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-20 17:31:06 -0700

    9. Learn your times table  (automated command)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * add - function that add to diffrenct numbers
+ * @x: int value
+ * @y: type int
+ *
+ * Return: the the result
+ *
+ */
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
