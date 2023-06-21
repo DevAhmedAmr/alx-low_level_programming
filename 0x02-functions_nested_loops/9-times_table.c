aa93a9742e0a80c7bb901dfd1c819660998eb6be
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-20 17:24:28 -0700

    9. Learn your times table  (automated command)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,38 +9,37 @@
  */
 void times_table(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i <= 9)
-    {
-        int j = 0;
+	while (i <= 9)
+	{
+		int j = 0;
 
-        while (j <= 9)
-        {
-            int result = i * j;
+		while (j <= 9)
+		{
+			int result = i * j;
 
-            if (j == 0)
-            {
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                if (result <= 9)
-                {
-                    _putchar(' ');
-                }
-                else
-                {
-                    _putchar((result / 10) + '0');
-                }
-                _putchar((result % 10) + '0');
-            }
-            j++;
-        }
-        _putchar('\n');
-        i++;
-    }
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result <= 9)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
