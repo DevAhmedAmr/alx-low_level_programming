f0d1fc17e5093f0f337f941b8b73ca5323a2e132
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-22 09:48:54 -0700

    test (automated command)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,20 @@
 /**
  * _isdigit - function that checks for a digit
+ * @c: type int input
  *
- * Return 0 succsess , else failed
+ * Description: function check
+ *
+ * Return: 0 succsess , else failed
  *
  */
 int _isdigit(int c)
 {
 	int i;
 
-	for (i=0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-	       	if (c == i)
-		return 1;
+		if (c == i)
+		return (1);
 	}
-	return 1;
+	return (0);
 }
