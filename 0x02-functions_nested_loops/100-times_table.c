e47ffc292760386394de0abbf17c873cbea24185
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-21 19:42:08 -0700

    test (automated command)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,43 +8,43 @@
  */
 void print_times_table(int n)
 {
-    if (n <= 15 && n >= 0)
-    {
-        int i = 0;
+	if (n <= 15 && n >= 0)
+	{
+		int i = 0;
 
-        for (; i <= n; i++)
-        {
-            int j = 0;
+		for (; i <= n; i++)
+		{
+			int j = 0;
 
-            for (; j <= n; j++)
+			for (; j <= n; j++)
             {
-                int result = i * j;
+		    int result = i * j;
 
-                if (result <= 9&&j!=0)
-                {
-                    _putchar(' ');
-                }
+		    if (result <= 9&&j!=0)
+		    {
+			    _putchar(' ');
+		    }
 
-                if (result <= 99 && j!=0)
-                {
-                    _putchar(' ');
-                }
-                if (result >= 100 )
-                {
-                    _putchar((result / 100) + '0');
-                    _putchar((result / 10) % 10 + '0');
-                }
-                else if (result <= 99 && result >= 10)
-                    _putchar((result / 10) + 48);
-                _putchar((result % 10) + 48);
+		    if (result <= 99 && j!=0)
+		    {
+			    _putchar(' ');
+		    }
+		    if (result >= 100 )
+		    {
+			    _putchar((result / 100) + '0');
+			    _putchar((result / 10) % 10 + '0');
+		    }
+		    else if (result <= 99 && result >= 10)
+			    _putchar((result / 10) + 48);
+		    _putchar((result % 10) + 48);
 
-                if (j != n)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-            }
-            _putchar('\n');
-        }
-    }
+		    if (j != n)
+		    {
+			    _putchar(',');
+			    _putchar(' ');
+		    }
+	    }
+			_putchar('\n');
+		}
+	}
 }
