e70aa5f129e438ec3615c70559db105cb0dc3b0b
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-21 23:03:23 -0700

    1. isdigit  (automated command)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - function that checks for a digit
+ *
+ * Return 0 succsess , else failed
+ *
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i=0; i<=9; i++)
+	{
+	       	if (i == 9)
+		return 1;
+	}
+	return 1;
+}
