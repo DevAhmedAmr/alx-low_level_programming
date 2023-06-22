3470bbdab41b2ff7e81cd91ad648eb730a5ed17e
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-21 19:55:22 -0700

    test (automated command)

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,31 +18,29 @@ void print_times_table(int n)
 
 			for (; j <= n; j++)
 			{
-		    int result = i * j;
+				int result = i * j;
 
-		    if (result <= 9 && j != 0)
-		    {
-			    _putchar(' ');
-		    }
-
-		    if (result <= 99 && j !=0 )
-		    {
-			    _putchar(' ');
-		    }
-		    if (result >= 100 )
-		    {
-			    _putchar((result / 100) + '0');
-			    _putchar((result / 10) % 10 + '0');
-		    }
-		    else if (result <= 99 && result >= 10)
-			    _putchar((result / 10) + 48);
-		    _putchar((result % 10) + 48);
-
-		    if (j != n)
-		    {
-			    _putchar(',');
-			    _putchar(' ');
-		    }
+				if (result <= 9 && j != 0)
+				{
+					_putchar(' ');
+				}
+				if (result <= 99 && j !=0 )
+				{
+					_putchar(' ');
+				}
+				if (result >= 100 )
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+					_putchar((result / 10) + 48);
+				_putchar((result % 10) + 48);
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			_putchar('\n');
 		}
