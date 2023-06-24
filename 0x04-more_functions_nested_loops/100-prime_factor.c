c2e0254478b537da68ada7cd26dbf9e5828852a5
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-23 19:22:47 -0700

    test (automated command)

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,70 +1,26 @@
 #include<stdio.h>
 /**
- * firstPrimeNumber - Finds the sammlest prime number
- * @n: takes an input
- *
- * description: function
- *
- * Return: smallest prime number
- *
- */
-
-
-unsigned long long firstPrimeNumber(unsigned long long n)
-{
-	unsigned long long i;
-
-	if (n > 1)
-	{
-		for (i = 2; i <= sqrt(n); i++)
-		{
-			if (n % i == 0)
-			{
-				return (i);
-			}
-		}
-	}
-	return (n);
-}
-
-
-/**
- * largestPrimeNum - finds the largest prime number
- * @n: input n
- *
- * description: function
- *
- * Return: largest prime number
- *
- */
-
-unsigned long long largestPrimeNum(unsigned long long n)
-{
-	while (1)
-	{
-		unsigned long long smallestPrimeNumber = firstPrimeNumber(n);
-
-		if (smallestPrimeNumber < n)
-		{
-			n = n / smallestPrimeNumber;
-		}
-		else
-		{
-			return (n);
-		}
-	}
-}
-
-/**
  * main - Entry point
  *
- * description: function
  *
- * Return: 0
+ * description: function print the largest prime factor
+ *
+ * Return: 0 if success
+ *
  */
 
 int main(void)
 {
-	printf("%lld\n", largestPrimeNum(612852475143));
+	long int number = 612852475143, i;
+
+	for (i = 2; i < number; i++)
+	{
+		while (number % i == 0)
+			number /= i;
+	}
+
+	printf("%ld\n", number);
+
 	return (0);
 }
+
