b23d7c5dcc19290d366b70a0b8944d0efa8426a6
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 06:36:55 -0700

    task puts 3  (automated command)

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,21 @@
 #include"main.h"
 /**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
  * _puts - function that print
  *
  * @str: input of type char
