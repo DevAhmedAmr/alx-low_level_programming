333a10bc48902a6ec86dcc7679a378186bfc89bc
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 07:12:54 -0700

    4. I can only go one way. I've not got a reverse gear  (automated command)

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,12 @@
+#include<string.h>
+/**
+ * print_rev - a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
