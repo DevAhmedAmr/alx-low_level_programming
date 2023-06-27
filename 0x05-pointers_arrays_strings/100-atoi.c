4ec99131d4c49eab145f52238d55d76dc38c288b
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 20:04:14 -0700

    test (automated command)

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,21 @@
 #include"main.h"
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+
 int _atoi(char *s)
 {
 	int operator= 1;
