94a90e13d9e1328ce4277da3914c3687d942ba22
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 18:08:00 -0700

    test (automated command)

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,7 +18,7 @@ int _strlen(char *s)
  * _strcpy - function that copies a string
  *
  * @dest: parameter of a string wanted to be copied to
- * @src: string wanted to be copy from 
+ * @src: string wanted to be copy from
  *
  * return: dest
  *
@@ -26,10 +26,11 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 
 {
-	int i =0;
+	int i = 0;
+
 	for (; i <= _strlen(src); i++)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
