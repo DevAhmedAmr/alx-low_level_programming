ed4b2ef19398cf39f161cb5ad6364444658785f8
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-27 04:18:39 -0700

    test (automated command)

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,32 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-
-int random_int(int min, int max)
+int getRandomAsciiNumber()
 {
-       int rand_num = rand();
+    int num = rand() % (122 - 48 + 1) + 32; /* generate a random number between 32 and 122 (inclusive)*/
+    printf("%i\n", num);
 
-        int range = max - min + 1;
-    int scaled_num = rand_num % range + min;
-
-    return scaled_num;
+    return num;
 }
-
-int main()
+int main(void)
 {
-    char allchars[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\0";
-    char passwd[100];
-    int allcharsLength = strlen(allchars) + 1;
-    int range = random_int(50, 100);
-    int i;
+    int i = 0;
+    int n = 0;
+    int sum = 0;
+    char password[100];
+    int totalSum = 0;
+    int random;
+    char asci;
+    srand(time(NULL)); /* seed the random number generator with current time*/
 
-        srand(time(NULL));
-
-    for (i = 0; i < range; i++)
+    for (i = 0; i < 2000; i++)
     {
-        passwd[i] = allchars[random_int(0, allcharsLength - 1)];    }
-    passwd[range] = '\0';
+        if (totalSum == 2772)
+        {
+            break;
+        }
+        if (totalSum < 2773 && totalSum > 2651)
+        {
+            int x = 2772 - totalSum;
+            totalSum += x;
+            asci = x;
+            password[i] = asci;
+        }
 
-    printf("%s", passwd);
-    return 0;
+        else if (totalSum < 2652)
+        {
+            random = getRandomAsciiNumber();
+            totalSum += random;
+            asci = random;
+            // printf("%c", asci);
+            password[i] = asci;
+        }
+        printf("total sum = %i", totalSum);
+    }
+    printf("%s , %i", password, totalSum);
+    return 1;
 }
