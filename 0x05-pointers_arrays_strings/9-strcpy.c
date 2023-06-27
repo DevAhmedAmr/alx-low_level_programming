31c3c4c80093931e87d8dfac390abc8a4ef61ac9
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 17:59:18 -0700

    test (automated command)

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+char *_strcpy(char *dest, char *src)
+
+{
+    for (int i = 0; i <= _strlen(src); i++)
+    {
+        dest[i] = src[i];
+    }
+    return dest;
+}
