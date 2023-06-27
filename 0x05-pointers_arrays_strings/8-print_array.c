6f31eb7f3fd9232b7e053e1c2d3eb4c2c7fba651
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 17:41:18 -0700

    test (automated command)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 void print_array(int *a, int n)
 {
-    for (int i = 0; i < n; i++)
+	int i;
+
+    for (i = 0; i < n; i++)
     {
         printf("%i", a[i]);
         if (i != n - 1)
