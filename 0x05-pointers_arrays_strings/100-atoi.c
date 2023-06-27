5b5cabf86b42c7b810210621ff13ba6c6e4aef5e
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 20:00:23 -0700

    test (automated command)

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include"main.h"
+int _atoi(char *s)
+{
+	int operator= 1;
+
+	int num = 0;
+
+	int i;
+
+	int ascii_code;
+
+	int value;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		ascii_code = (int)s[i];
+
+		if (ascii_code == 45)
+		
+
+			operator*= - 1;
+		
+		else if (ascii_code == 43)
+
+			operator*= 1;
+		
+
+		if (num > 0 && (ascii_code < 47) || num > 0 && ascii_code > 58)
+			break;
+		else if ((ascii_code < 47) || ascii_code > 58)
+			continue;
+		if (ascii_code > 47 && ascii_code < 58)
+		{
+			value = ascii_code - '0';
+			num *= 10;
+			num += value;
+		}
+	}
+	return (num *= operator);
+}
+
