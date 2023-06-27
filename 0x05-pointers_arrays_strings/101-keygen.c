6bd1f54d1b0f73062c2da37612bd9484265b03de
Ahmed <ahmedamr989dyhdhdhd@gmail.com>
2023-06-26 19:21:23 -0700

    test (automated command)

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#define PASSWORD_LENGTH 8
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+    char passwd[PASSWORD_LENGTH + 1];
+    const char charcters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int len = sizeof(charcters) - 1;
+    srand(time(NULL));
+   
+    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        passwd[i] = charcters[rand() % len];
+    }
+    passwd[len] = '\0';
+
+    printf("%s\n", passwd);
+    return 0;
+}
