c03631d5637cdec917bb7609550c6b5fc801198d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 09:56:12 -0700

    task 2 ,2. strncpy  (automated command)

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -26,16 +26,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	int len = _strlen(src);
-
-	if (len < n)
-		n = len;
-	if (n < 0)
-		n = 0;
-
-	for (i = 0; i < n; i++)
+	for (i = 0; (i < n) && (src[i] != '\0'); i++)
+	{
 		dest[i] = src[i];
-	dest[_strlen(dest)]='\0';
-	
-	return (dest);
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (des)t;
 }
