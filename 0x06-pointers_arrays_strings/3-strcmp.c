247252074cfddf388b867c2cc09e7f3d23d41127
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 10:37:32 -0700

    task 2 ,2. strncpy  (automated command)

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include"main.h"
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * _strlen - function compare strings lenth
+ * @s1: string1 
+ * @s2: string1
+ *
+ * Return: If s1 is less than s2, the function returns a negative integer.
+ * If s1 is greater than s2, the function returns a positive integer.
+ * If s1 is equal to s2, the function returns zero.
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+        len++;
+    return (len);
+}
