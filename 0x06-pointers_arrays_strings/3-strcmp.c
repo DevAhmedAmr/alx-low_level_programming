5637ab55e8996f2386a1a0a6f6bc9d518cde007d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 12:01:01 -0700

    =task3 (automated command)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strlen(char *s)
 	return (len);
 }
 /**
- * _strlen - function compare strings lenth
+ * _strcmp - function compare strings lenth
  * @s1: string1 
  * @s2: string1
  *
@@ -23,7 +23,7 @@ int _strlen(char *s)
  * If s1 is greater than s2, the function returns a positive integer.
  * If s1 is equal to s2, the function returns zero.
  */
-int _strcm2p(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
 	{
