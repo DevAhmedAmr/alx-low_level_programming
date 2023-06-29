285212dbf042396e44e77890ca3501bad7ff6187
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 12:15:31 -0700

    =task3 (automated command)

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i = (n - 1), j = 0;
+	int i = (n - 1), j = 0;
 
-    for (; i >= n / 2; i--, j++)
-    {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
+	for (; i >= n / 2; i--, j++)
+	{
+		int temp = a[i];
+
+		a[i] = a[j];
+		a[j] = temp;
+	}
 }
