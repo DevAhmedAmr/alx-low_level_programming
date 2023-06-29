9a567ea8b477188a13954ebbd52fb62796b2e5a8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 11:20:29 -0700

    =task3 (automated command)

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+void reverse_array(int *a, int n)
+{
+	for(i = n ;i >= 0 ; i++)
+	{
+		
+
+	}
+}
