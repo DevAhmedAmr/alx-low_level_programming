dfe2af3648299e0c24e1f56d5b657dd5ac962acb
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-30 16:32:55 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,87 +17,84 @@ int _strlen(char *s)
 
 void intToString(int num, char *str)
 {
-    int isNegative = 0;
-    int i = 0;
-    int length;
-    int j;
+	int isNegative = 0;
+	int i = 0;
+	int length;
+	int j;
 
-    if (num == 0)
-    {
-        str[0] = '0';
-        str[1] = '\0';
-        return;
-    }
-
-    if (num < 0)
-    {
+	if (num == 0)
+	{
+		str[0] = '0';
+		str[1] = '\0';
+		return;
+	}
+	if (num < 0)
+	{
         isNegative = 1;
         num = -num;
-    }
+	}
 
-    while (num != 0)
-    {
-        int digit = num % 10;
-        str[i++] = digit + '0';
-        num /= 10;
-    }
+	while (num != 0)
+	{
+		int digit = num % 10;
+		str[i++] = digit + '0';
+		num /= 10;
+	}
 
-    if (isNegative)
-    {
-        str[i++] = '-';
-    }
+	if (isNegative)
+	{
+		str[i++] = '-';
+	}
 
-    str[i] = '\0';
+	str[i] = '\0';
 
     /* Reverse the string */
 
-    length = i;
+	length = i;
 
-    for (j = 0; j < length / 2; j++)
-    {
-        char temp = str[j];
-        str[j] = str[length - j - 1];
-        str[length - j - 1] = temp;
-    }
+	for (j = 0; j < length / 2; j++)
+	{
+		char temp = str[j];
+		str[j] = str[length - j - 1];
+		str[length - j - 1] = temp;
+	}
 }
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int num1 = 0;
-    int num2 = 0;
-    int result;
+	int num1 = 0;
+	int num2 = 0;
+	int result;
 
-    int len1 = _strlen(n1);
-    int len2 = _strlen(n2);
-    int i, j;
+	int len1 = _strlen(n1);
+	int len2 = _strlen(n2);
+	int i, j;
 
-    if (len1 + len2 + 1 > size_r)
-    {
-        return 0;
-    }
+	if (len1 + len2 + 1 > size_r)
+	{
+		return 0;
+	}
 
-    for (i = 0; i < len1; i++)
-    {
-        num1 = num1 * 10;
-        num1 += (n1[i] - '0');
-    }
+	for (i = 0; i < len1; i++)
+	{
+		num1 = num1 * 10;
+		num1 += (n1[i] - '0');
+	}
 
-    for (j = 0; j < len2; j++)
-    {
-        num2 = num2 * 10;
-        num2 += (n2[j] - '0');
-    }
+	for (j = 0; j < len2; j++)
+	{
+		num2 = num2 * 10;
+		num2 += (n2[j] - '0');
+	}
+	result = num1 + num2;
 
-    result = num1 + num2;
+	if (result < 0) {
+		result *= -1;
+		r[0] = '-';
+		intToString(result, r+1);
+	} else {
+		intToString(result, r);
+	}
+	r[_strlen(r)] = '\0';
 
-    if (result < 0) {
-        result *= -1;
-        r[0] = '-';
-        intToString(result, r+1);
-    } else {
-        intToString(result, r);
-    }
-
-    r[_strlen(r)] = '\0';
-
-    return r;
+	return r;
 }
