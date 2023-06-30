a54de882f53fa504c53cf9fff3dd467ed19fe795
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-06-29 19:00:35 -0700

    7. Mozart composed his music not for the elite, but for everybody  (automated command)

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
+ * leet - function that encodes a string into 1337.
+ * Mozart composed his music not for the elite,
+ * but for everybody
+ * @str: string that wanted to be encoded
+ * Return: str
+ */
+
+
+char *leet(char *str)
+{
+	char charsList[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	char charsToNumbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (int i = 0; i < _strlen(str); i++)
+	{
+		for (size_t j = 0; j < 10 && str[i] != '\0'; j++)
+		{
+			if (str[i] == charsList[j])
+			{
+				str[i] = charsToNumbers[j];
+			}
+		}
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
 char *cap_string(char *str);
+char *leet(char *str);
 #define MAX_SIZE 100
 
 #endif
