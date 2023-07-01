5a56445b96e0dfb1e8b2681686464ed766ac2db7
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 02:23:59 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,19 @@
 /**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+/**
  * rot13 - Applies the ROT13 substitution cipher to a string
  * Replaces each letter in the string with the letter 13 positions
  * ahead of it in the alphabet, wrapping around from 'z' to 'a' or
