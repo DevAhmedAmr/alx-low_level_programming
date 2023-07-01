b064897e29acd7e9dbfa020aba8d60a62674fa18
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 03:42:14 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -61,41 +61,46 @@ void intToString(int num, char *str)
 }
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num1 = 0;
-	int num2 = 0;
-	int result;
-	char *ptr;
-	int len1 = _strlen(n1);
-	int len2 = _strlen(n2);
-	int i, j;
+    int num1 = 0;
+    int num2 = 0;
+    int result;
+    int len1 = _strlen(n1);
+    int len2 = _strlen(n2);
+    int i, j;
 
-	if (len1 + len2 + 1 > size_r)
-	{
-		return 0;
-	}
+    if (len1 + len2 + 1 > size_r)
+    {
+        return 0;
+    }
 
-	for (i = 0; i < len1; i++)
-	{
-		num1 = num1 * 10;
-		num1 += (n1[i] - '0');
-	}
+    for (i = 0; i < len1; i++)
+    {
+        num1 = num1 * 10;
+        num1 += (n1[i] - '0');
+    }
 
-	for (j = 0; j < len2; j++)
-	{
-		num2 = num2 * 10;
-		num2 += (n2[j] - '0');
-	}
-	result = num1 + num2;
+    for (j = 0; j < len2; j++)
+    {
+        num2 = num2 * 10;
+        num2 += (n2[j] - '0');
+    }
+    result = num1 + num2;
 
-	if (result < 0) {
-		result *= -1;
-		r[0] = '-';
-		intToString(result, r+1);
-	} else {
-		intToString(result, r);
-	}
-	r[_strlen(r)] = '\0';
-	ptr = r;
+    if (result < 0)
+    {
+        result *= -1;
+        r[0] = '-';
+        intToString(result, r + 1);
+    }
+    else if (result > (int)2147483647)
+    {
+        return 0;
+    }
+    else
+    {
+        intToString(result, r);
+    }
+    r[_strlen(r)] = '\0';
 
-	return ptr;
+    return r;
 }
