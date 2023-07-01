e210be3756573d03b661b8dbd0e63d8eb0404ebc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 04:29:28 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -59,48 +59,52 @@ void intToString(int num, char *str)
 		str[length - j - 1] = temp;
 	}
 }
+/**
+ * infinite_add - Adds two numbers.
+ * @n1: The first number to add.
+ * @n2: The second number to add.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
+ * Return: A pointer to the result, or 0 if the result cannot be stored in r.
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int num1 = 0;
-    int num2 = 0;
-    int result;
-    int len1 = _strlen(n1);
-    int len2 = _strlen(n2);
-    int i, j;
+    int len1 = 0, len2 = 0, len_sum = 0, carry = 0, sum = 0, i = 0, j = 0;
 
+    while (n1[len1] != '\0') /* Calculate length of n1 */
+        len1++;
+    while (n2[len2] != '\0') /* Calculate length of n2 */
+        len2++;
+
+    /* Check if the sum of n1 and n2 can fit in r */
     if (len1 + len2 + 1 > size_r)
+        return (0);
+
+    /* Add digits from right to left */
+    for (i = len1 - 1, j = len2 - 1, len_sum = 0; i >= 0 || j >= 0 || carry; i--, j--)
     {
-        return 0;
+        sum = carry;
+
+        if (i >= 0)
+            sum += n1[i] - '0';
+        if (j >= 0)
+            sum += n2[j] - '0';
+
+        r[len_sum] = sum % 10 + '0';
+        carry = sum / 10;
+        len_sum++;
     }
 
-    for (i = 0; i < len1; i++)
+    /* Reverse the string in r */
+    for (i = 0, j = len_sum - 1; i < j; i++, j--)
     {
-        num1 = num1 * 10;
-        num1 += (n1[i] - '0');
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
     }
 
-    for (j = 0; j < len2; j++)
-    {
-        num2 = num2 * 10;
-        num2 += (n2[j] - '0');
-    }
-    result = num1 + num2;
+    r[len_sum] = '\0';
 
-    if (result < 0)
-    {
-        result *= -1;
-        r[0] = '-';
-        intToString(result, r + 1);
-    }
-    else if (result > (int)2147483647)
-    {
-        return 0;
-    }
-    else
-    {
-        intToString(result, r);
-    }
-    r[_strlen(r)] = '\0';
-
-    return r;
+    return (r);
 }
