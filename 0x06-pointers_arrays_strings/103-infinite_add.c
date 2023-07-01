8ff1b2abb1014707d78b51650c966ef04ae8adc4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 08:42:32 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,19 +10,22 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int len1 = 0, len2 = 0, len_sum = 0, carry = 0, i = 0, j = 0;
 
+    char *result_ptr;
+
     for (i = 0; n1[i]; i++)
         ;
     len1 = i;
     for (j = 0; n2[j]; j++)
         ;
     len2 = j;
+
     /* Check if the sum of n1 and n2 can fit in r */
     if (len1 > size_r || len2 > size_r)
     {
         return 0;
     }
-    /* Add digits from right to left */
 
+    /* Add digits from right to left */
     for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--, len_sum++)
     {
         int digit_n1 = i >= 0 ? n1[i] - '0' : 0;
@@ -35,7 +38,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
     r[len_sum] = '\0';
 
     if (carry)
-
         return 0;
 
     /* Reverse the string in r */
@@ -46,5 +48,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
         r[j] = temp;
     }
 
-    return (r);
+    /* Declare a pointer variable and assign the address of the result buffer to it */
+    result_ptr = r;
+
+    /* Return the pointer variable */
+    return result_ptr;
 }
