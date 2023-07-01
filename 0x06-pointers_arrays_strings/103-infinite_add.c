4f1d02c9f499b3d8bf00d640e1f803b59aa6db93
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 08:33:12 -0700

    first commit (automated command)

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,13 +8,14 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, len_sum = 0, carry = 0, i = 0, j = 0;
+    int len1 = 0, len2 = 0, len_sum = 0, carry = 0, sum = 0, i = 0, j = 0;
 
-    while (n1[len1] != '\0') /* Calculate length of n1 */
-        len1++;
-    while (n2[len2] != '\0') /* Calculate length of n2 */
-        len2++;
-
+    for (i = 0; n1[i]; i++)
+        ;
+    len1 = i;
+    for (j = 0; n2[j]; j++)
+        ;
+    len2 = j;
     /* Check if the sum of n1 and n2 can fit in r */
     if (len1 > size_r || len2 > size_r)
     {
