e4cb49add5a03ea88d663f82e36fdbfc04a0640a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-01 22:04:50 -0700

    added my own 103- infintyadd (automated command)

--- 0x06-pointers_arrays_strings/myOwn103-infinty_add.c
+++ 0x06-pointers_arrays_strings/myOwn103-infinty_add.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 /*impelementation of a calculator but its limited by unsigned long or int*/
 /* since Long Long is not allowed in the alx 0x006 more pointer and array*/
 /* task*/
@@ -49,7 +50,68 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[resultLen - i - 1] = x + '0';
 		result /= 10;
 	}
+
+	/* another way to convert from int to string */
+
+	/* * for (i = resultLen; i >= 0; i--)*/
+	/* {*/
+	/* int x = result % 10;*/
+	/* r[i - 1] = x + '0';*/
+	/* result /= 10;*/
+	/* }*/
+
 	r[resultLen] = '\0';
 
 	return (r);
+}
+int main(void)
+{
+    char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+    char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+    char r[100];
+    char r2[10];
+    char r3[11];
+    char *res;
+
+    res = infinite_add(n, m, r, 100);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+    n = "1234567890";
+    m = "1";
+    res = infinite_add(n, m, r2, 10);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+    n = "2000000000";
+    m = "2000000000";
+    res = infinite_add(n, m, r2, 10);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+    res = infinite_add(n, m, r3, 11);
+    if (res == 0)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("%s + %s = %s\n", n, m, res);
+    }
+    return (0);
 }
