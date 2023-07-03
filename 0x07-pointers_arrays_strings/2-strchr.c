da2f2d0d99233888427c0bc6608cdbf5b952dc90
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 04:42:17 -0700

    first commit (automated command)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 char *_strchr(char *s, char c)
 {
-    while (*s != c)
+    while (*s != '\0') {
+        if (*s == c) {
+            return s;
+        }
         s++;
-    if (*s == c)
-	    return s;
-       return NULL;
+    }
 
+    return NULL;
 }
