8b2f2d2afb622d02caf3073aa3a508b1ecbbc8df
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 15:09:14 -0700

    added documentation for task 4. strpbrk (automated command)

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
 #define MAX_SIZE 100
 
 #endif
