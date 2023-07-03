778532a5fe5d75886e2714e984c6278f6e3c97f4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 09:01:06 -0700

    first commit (automated command)

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int sLen = 0, acceptLen = 0, x = 0, j, i;
+    while (s[sLen] != '\0')
+    {
+        sLen++;
+    }
+    while (accept[acceptLen] != '\0')
+    {
+        acceptLen++;
+    }
+
+    for (i = 0; i < sLen; i++)
+    {
+
+        for (j = 0; j < acceptLen; j++)
+        {
+            /*printf("s %c , accept %c\n", s[i], accept[j]);*/
+
+            if (s[i] == accept[j])
+            {
+                x = 1;
+                break;
+            }
+            x = 0;
+        }
+        /* printf("-----------\n");*/
+
+        if (x == 0)
+        {
+            return i;
+        }
+    }
+
+    return i;
+}
