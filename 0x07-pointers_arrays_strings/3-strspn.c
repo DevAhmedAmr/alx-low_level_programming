51cb8fdb7b3f20ee8e20d17b298f15037c1e79e1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 09:14:25 -0700

    first commit (automated command)

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,70 +1,31 @@
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int sLen = 0, acceptLen = 0, x = 0, j, i;
-while (s[sLen] != '\0')
-{
-sLen++;
-}
-while (accept[acceptLen] != '\0')
-{
-acceptLen++;
-}
+	unsigned int sLen = 0, acceptLen = 0, is_match  = 0, j, i;
+	/*get s variable length*/
+	while (s[sLen] != '\0')
+		sLen++;
 
-ini
-Copy
-for (i = 0; i < sLen; i++)
-{
+	/*get accept variable length*/
+	while (accept[acceptLen] != '\0')
+		acceptLen++;
 
-    for (j = 0; j < acceptLen; j++)
-    {
-
-        if (s[i] == accept[j])
-        {
-            x = 1;
-            break;
-        }
-        x = 0;
-    }
-    /* printf("-----------\n");*/
-
-    if (x == 0)
-    {
-        return i;
-    }
-}
-
-return i;
-}
-unsigned int _strspn(char *s, char *accept)
-{
-unsigned int sLen = 0, acceptLen = 0, is_match = 0, j, i;
-/get s variable length/
-while (s[sLen] != '\0')
-sLen++;
-
-ini
-Copy
-/*get accept variable length*/
-while (accept[acceptLen] != '\0')
-	acceptLen++;
-
-for (i = 0; i < sLen; i++)
-{
-
-	for (j = 0; j < acceptLen; j++)
+	for (i = 0; i < sLen; i++)
 	{
-		if (s[i] == accept[j])
+
+		for (j = 0; j < acceptLen; j++)
 		{
-			is_match  = 1;
-			break;
+			if (s[i] == accept[j])
+			{
+				is_match  = 1;
+				break;
+			}
+			is_match  = 0;
 		}
-		is_match  = 0;
+		if (is_match  == 0)
+		{
+			return i;
+		}
 	}
-	if (is_match  == 0)
-	{
-		return i;
-	}
-}
 
-return i;
+    return i;
 }
