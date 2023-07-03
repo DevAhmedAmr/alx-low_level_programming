46af5725dc47ccc718057683a55c7d46b6e3e585
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 10:27:03 -0700

    added documentation for task 4. strpbrk (automated command)

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,39 @@
 #include <stddef.h>
+/**
+ * _strpbrk - Finds the first occurrence of any character from the
+ * 'accept' string in the 's' string then return a ptr to it
+ *
+ * paramters:
+ * @s: A pointer to the input string.
+ * @accept: A pointer to the set of accepted characters.
+ *
+ * Return: A pointer to the first occurrence of a matching character in 's'
+ * , or return null if no match
+ */
 char *_strpbrk(char *s, char *accept)
 {
-    unsigned int sLen = 0, acceptLen = 0, j, i;
-    /*get s variable length*/
-    while (s[sLen] != '\0')
-        sLen++;
+	unsigned int sLen = 0, acceptLen = 0, j, i;
 
-    /*get accept variable length*/
-    while (accept[acceptLen] != '\0')
-        acceptLen++;
+	/* Get the length of the 's' variable */
+	while (s[sLen] != '\0')
+		sLen++;
 
-    for (i = 0; i < sLen; i++)
-    {
+	/* Get the length of the 'accept' variable */
+	while (accept[acceptLen] != '\0')
+		acceptLen++;
 
-        for (j = 0; j < acceptLen; j++)
-        {
-            if (*s == accept[j])
-            {
-                return s;
-            }
-        }
-        s++;
-    }
-    return NULL;
+	for (i = 0; i < sLen; i++)
+	{
+		for (j = 0; j < acceptLen; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
 }
+
