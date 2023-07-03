c578cc961429a1eec7c3a77ad10fb0e4b3b212cd
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 09:28:41 -0700

    first commit (automated command)

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,15 @@
+/**
+ * _strspn - Calculates the length of the initial segment of a string that
+ * consists of only characters from a specified set of accepted characters.
+ *
+ * parameters:
+ * @s: A pointer to the input string.
+ * @accept: A pointer to the set of accepted characters.
+ *
+ * Return: The length of the initial segment of the string.
+ * if all the set of 'accepted chars' in the 's' variable
+ * will return full length of 's'
+ */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int sLen = 0, acceptLen = 0, is_match  = 0, j, i;
@@ -9,6 +21,7 @@ unsigned int _strspn(char *s, char *accept)
 	while (accept[acceptLen] != '\0')
 		acceptLen++;
 
+	/*compare 2 strings and return*/
 	for (i = 0; i < sLen; i++)
 	{
 
@@ -19,13 +32,14 @@ unsigned int _strspn(char *s, char *accept)
 				is_match  = 1;
 				break;
 			}
+
+			/*if 's' contain a char that not in 'accept' */
 			is_match  = 0;
 		}
 		if (is_match  == 0)
 		{
-			return i;
+			return (i);
 		}
 	}
-
-    return i;
+	return (i);
 }
