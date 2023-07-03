b17946167370edbfac4f36ee1d9d5b4808c756f3
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 00:28:38 -0700

    first commit (automated command)

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: the string that wanted to be filled the byte
+ * @b: the char that the s wanted to be filled with
+ * @n: size of s
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	s[n] = '\0';
+	return (s);
+}
