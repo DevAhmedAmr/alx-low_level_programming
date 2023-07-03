a9f9ccf6482061026b090c9ee4da5e89c4faacac
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 02:49:18 -0700

    first commit (automated command)

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,10 +7,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
+	{
 		s[i] = b;
+		i++;
+	}
 	s[n] = '\0';
 	return (s);
 }
