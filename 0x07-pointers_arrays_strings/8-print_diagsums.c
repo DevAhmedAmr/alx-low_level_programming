b9faf4f0a10c25601bc1148106f0e6aa362531b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 14:07:15 -0700

    added documentation for task 4. strpbrk (automated command)

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-void print_diagsums(int *a, int size)
+#include <stdio.h>
+
+void print_diagsums(int* a, int size)
 {
-    int i, j;
-    unsigned long rightDiagonals = 0, leftDiagonals = 0;
-    int (*p)[size] = (int(*)[size])a;
+	int i;
+	unsigned long rightDiagonals = 0, leftDiagonals = 0;
 
-    for (i = 0, j = 0; i < size; i++, j++)
-    {
-        rightDiagonals += p[i][j];
-    }
+	for (i = 0; i < size; i++)
+	{
+		rightDiagonals += a[i * size + i];
+		leftDiagonals += a[i * size + (size - 1 - i)];
+	}
 
-    for (i = 0, j = size - 1; i < size; i++, j--)
-    {
-        leftDiagonals += p[i][j];
-    }
-
-    printf("%lu, %lu\n", rightDiagonals, leftDiagonals);
+	printf("%lu, %lu\n", rightDiagonals, leftDiagonals);
 }
 
