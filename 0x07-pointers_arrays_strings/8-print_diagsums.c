309dc9be8efe8888806104faf9145ac3bd03bc8e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-03 14:23:18 -0700

    added documentation for task 4. strpbrk (automated command)

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+/**
+ * print_diagsums - function that prints the sum of the two diagonals
+ * of a square matrix of integers
+ *
+ * parameters:
+ * @a: paramter of type pointer to an intiger array
+ * @size: size of the array = size * size 
+ * exp: 3 * 3 
+ *
+ * Return: void;
+ */
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int rightDiagonals = 0, leftDiagonals = 0;
+	int i;
 
-    for (i = 0; i < size; i++)
-    {
-        rightDiagonals += a[i * size + i];
-        leftDiagonals += a[i * size + (size - 1 - i)];
-    }
+	int rightDiagonals = 0, leftDiagonals = 0;
 
-   printf("%i, %i\n", rightDiagonals, leftDiagonals);
+	for (i = 0; i < size; i++)
+	{
+		rightDiagonals += a[i * size + i];
+		leftDiagonals += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%i, %i\n", rightDiagonals, leftDiagonals);
 }
