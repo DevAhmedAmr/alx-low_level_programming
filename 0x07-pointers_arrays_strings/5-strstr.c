5ec705fd4175c15a42f2ce93915b968dc811e6b0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-04 07:06:11 -0700

    first commit (automated command)

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,45 +16,57 @@ int _strlen(char *s)
 }
 
 /**
- * _strstr - function that search for a string in another string
+ * _strstr - locates the first occurrence of a substring in another string
+ *
  *
  * parameters:
- * @haystack: string that will be searched in
- * @needle: string that will be searched for
- * Return: a pointer to needle if found
- * if not found return null
+ * @haystack: the string to search within
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the matching substring, or NULL if no
+ * match is found
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-    int isMatch = 0;
-    int matchIndex = -1;
-    int i, j;
-    for (i= 0; i < _strlen(haystack); i++)
-    {
-       
-        isMatch = 1;
-        for ( j = 0; j < _strlen(needle); j++)
-        {
-            if (needle[j] != haystack[i + j])
-            {
+int isMatch = 0;    /* Initialize a flag to indicate if a match is found */
+int matchIndex = -1, i, j;
 
-                isMatch = 0;
-                break;
-            }
-        }
+	/* Loop through each character in 'haystack' */
+	for (i = 0; i < _strlen(haystack); i++)
+	{
+		isMatch = 1;        /* Assume a match is found until proven otherwise */
+		/* Loop through each character in 'needle' */
 
-        if (isMatch == 1)
-        {
-            matchIndex = i; /* Store the starting index of the match*/
+		for (j = 0; j < _strlen(needle); j++)
+		{
+/* Compare the characters at the same index in  both strings */
+/* 'j' index in 'haystack[j + i]' && needle[j] will only be incremented */
+/* if the loop is not broken that means j will only be more than '0' */
+/* if (needle[j] != haystack[j + i]) is false*/
 
-            break;
-        }
-    }
-    if (matchIndex != -1)
-    {
+			if (needle[j] != haystack[i + j])
+			{
+/* If a mismatch is found, set the flag to false and break the loop */
+				isMatch = 0;
 
-        return &haystack[matchIndex]; /* Return a pointer to the matching substring*/
-    }
-    return (NULL);
+				break;
+			}
+		}
+
+/* If a match is found, store i in match index and break the loop */
+		if (isMatch == 1)
+		{
+			matchIndex = i;
+			break;
+		}
+	}
+
+/* If a match is found, return a pointer to the matching substring;*/
+/* otherwise, return NULL */
+	if (matchIndex != -1)
+	{
+		return (&haystack[matchIndex]);
+	}
+	return (NULL);
 }
