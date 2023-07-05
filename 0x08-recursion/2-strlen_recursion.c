b9b3a4f484c0a1af7b117d44b6af5d91357c1523
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 08:13:24 -0700

    task 3 lenght by recursive (automated command)

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,22 @@
+/**
+ * _strlen_recursion - a furnction that return the length of a string by
+ * recursive
+ *
+ * parameters:
+ * @s: type string
+ *
+ * Return: length of a string
+ */
 int _strlen_recursion(char *s)
 {
-    int len = 0;
-    if (*s != '\0')
+	int len = 0;
 
-    {
-        len++;
-        len += _strlen_recursion(s + 1);
-    }
-    return len;
+	if (*s != '\0')
+
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }
 
