0aee059cf0c6d3da00010e5fca09e2df29274caa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 08:41:08 -0700

    task 3 lenght by recursive (automated command)

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,20 @@
+/**
+ * factorial - function that return factorial of a given number
+ *
+ * parameters:
+ * @n: the number that it's factorial wanted to be calculated
+ *
+ * Return: factorial of the given number
+ */
 int factorial(int n)
 {
-    if (n == 1)
-    {
-        return 1;
-    }
-    else if (n == 0)
-    {
-        return 0;
-    }
-    else if (n < 0)
-    {
-        return -1;
-    }
-
-    if (n > 2)
-    {
-
-        return n * factorial((n - 1));
-    }
-    return n;
+	if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	if (n > 2)
+		return (n * factorial((n - 1)));
+	return (n);
 }
