b5f3a5846ad4324b3ec59c2da5efe703e5d4cb52
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 06:35:56 -0700

    first commit (automated command)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-
-    if (*s != '\0')
-    {
-        write(1, s, 1);
-        s++;
-        _puts_recursion(s);
-    }
-    else
-        write(1, "\n", 1);
+	if (*s != '\0')
+	{
+		write(1, s, 1);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		write(1, "\n", 1);
 }
