1601e11b437ecf6ef171d6cede37474560c3276e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 07:02:45 -0700

    first commit (automated command)

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,10 @@
 #include"main.h"
 void _print_rev_recursion(char *s)
 {
-     if (*s != '\0')
+    if (*s != '\0')
     {
 
-        _print_rev_recursion(++s);
-        _putchar(*s);
+        _print_rev_recursion(s + 1);
+        putchar(*s);
     }
 }
-
