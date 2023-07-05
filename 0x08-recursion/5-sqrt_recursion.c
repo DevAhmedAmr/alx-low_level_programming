dd5285b26328f5c387e65a56b5e2f9ea30ca54aa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 13:13:36 -0700

    5. Your subconscious is looking for the dreamer (automated command)

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,17 @@
 int sqrtHelper(int num, int n);
+
 int _sqrt_recursion(int n)
 {
-    int num = 0;
-
-    return sqrtHelper(num, n);
+	int num = 0;
+	return sqrtHelper(num, n);
 }
+
 int sqrtHelper(int num, int n)
 {
-    if (num > n)
-    {
-        return -1;
-    }
+	if (num > n)
+		return -1;
 
-    if (num * num != n)
-    {
-        return sqrtHelper(num + 1, n);
-    }
-
-    return num;
+	if (num * num != n)
+		return sqrtHelper(num + 1, n);
+	return num;
 }
