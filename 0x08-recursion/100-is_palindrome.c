5e7d9bd206d83ba08a06a140955f366d4ca70928
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 15:45:56 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything  (automated command)

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,36 +1,67 @@
+/**
+ * _strlen_recursion - a furnction that return the length of a string by
+ * recursive
+ *
+ * parameters:
+ * @s: type string
+ *
+ * Return: length of a string
+ */
 int _strlen_recursion(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    if (*s != '\0')
-
-    {
-        len++;
-        len += _strlen_recursion(s + 1);
-    }
-    return (len);
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }
+/**
+ * compare - a helper function that compare string charcters to determine
+ * if the string is a palindrome or not
+ *
+ * parameters:
+ * @start: starting index
+ * @end: length or the last letter in the arr
+ * @str: the string that we want to know if its palindrome  or not
+ *
+ * Return: 1 if palindrome otherwise 0
+ */
+
 int compare(int start, int end, char *str)
 {
-
-    if (str[start] == str[end] && str[start] != '\0' && str[end] != '0')
-    {
-
-        return compare(start + 1, end - 1, str);
-    }
-    else if ((str[start] != str[end]))
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
+	if (str[start] == str[end] && str[start] != '\0' && str[end] != '0')
+	{
+		return (compare(start + 1, end - 1, str));
+	}
+	else if (str[start] != str[end])
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
+
+/**
+ * is_palindrome - a function that takes a string and determine
+ * if its is_palindrom or not
+ *
+ * parameters:
+ * @s: the string that we want to know if its palindrome  or not
+ *
+ * Return: 1 if palindrome otherwise 0
+ */
 
 int is_palindrome(char *s)
 {
-    int end = _strlen_recursion(s) - 1;
-    int start = 0;
-    return compare(start, end, s);
+	int end = _strlen_recursion(s) - 1;
+
+	int start = 0;
+
+	return (compare(start, end, s));
 }
+
