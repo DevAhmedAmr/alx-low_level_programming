3611da5bd76823bb4c2df5a048a7a25d747cad97
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 09:14:26 -0700

    task 4. Once an idea has taken hold of the brain it's almost impossible to eradicate  (automated command)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,11 @@
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return -1;
-    }
-    else if (y == 0)
-    {
-        return 1;
-    }
-
-    if (y > 0 && y != 1)
-    {
-        return x * _pow_recursion(x, y - 1);
-    }
-    return x;
+	if (y < 0)
+		return -1;
+	else if (y == 0)
+		return 1;
+	if (y > 0 && y != 1)
+		return x * _pow_recursion(x, y - 1);
+       	return x;
 }
 
