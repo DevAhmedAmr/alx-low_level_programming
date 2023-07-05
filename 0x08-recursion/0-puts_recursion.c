9828dfc0415e3dcc2bd90d0e01ede574ffd97f7e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 06:16:42 -0700

    first commit (automated command)

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+/**
+ * _puts_recursion - a function that takes an string and print if without loops
+ * paramter:
+ * @s: type string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	write(1, s, 1);
+	s++;
+
+	if (*s != '\0')
+
+		_puts_recursion(s);
+	else
+		write(1, "\n", 1);
+}
+
