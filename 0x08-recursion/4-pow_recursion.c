babf4bce1c1d148e4a799e35f3ce6c3daeb1516f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-05 14:18:57 -0700

    6. Inception. Is it possible?  (automated command)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,21 @@
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * then return it.
+ *
+ * @x: parm of The base value that wanted to be calcualted.
+ * @y:parm of The exponent value that wanted to be raised.
+ *
+ * Return: The value of x raised to the power of y,
+ * or -1 if y is negative.
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 	else if (y == 0)
-		return 1;
+		return (1);
 	if (y > 0 && y != 1)
-		return x * _pow_recursion(x, y - 1);
-       	return x;
+		return (x * _pow_recursion(x, y - 1));
+	return (x);
 }
 
