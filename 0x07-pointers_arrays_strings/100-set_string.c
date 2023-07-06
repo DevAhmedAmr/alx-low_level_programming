b6966685185f911e162ac76e4a5a8bd86f3402b8
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-06 09:15:37 -0700

    8. Double pointer, double fun  (automated command)

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,14 @@
+/**
+ * set_string -  functio that takes a double pointer and changes
+ * its dereferenced value
+ *
+ * parameters:
+ * @s: a double pointer to a string that we want to change its value
+ * @to: the string that we want to change the value to
+ *
+ * Return: void
+ */
 void set_string(char **s, char *to)
 {
-    *s = to;
+	*s = to;
 }
