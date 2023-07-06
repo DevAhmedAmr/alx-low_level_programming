7eca17c8618af0304d84f7fb0f1e34fd793221f4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-06 09:08:58 -0700

    8. Double pointer, double fun  (automated command)

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+void set_string(char **s, char *to);
 #define MAX_SIZE 100
 
 #endif
