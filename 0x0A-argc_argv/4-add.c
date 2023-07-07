30983158d030493f48433e69fa186b0832422e6d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-07 15:29:50 -0700

    4. To infinity and beyond  (automated command)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,6 +15,15 @@ int _strlen(char *s)
 		len++;
 	return (len);
 }
+/**
+ * stringToInt - function that convert string to int
+ *
+ * paramters:
+ * @num: the buffer
+ * @str: the number in the string form
+ *
+ * Return: 0 if succses , 1 if the string contains a symbol or character
+ */
 
 int stringToInt(int *num, char *str)
 {
@@ -32,16 +41,24 @@ int stringToInt(int *num, char *str)
 		*num *= 10;
 		if ((int)str[i] < 48 || (int)str[i] > 57)
 		{
-			return 1;
+			return (1);
 		}
 		*num += (((int)str[i]) - 48);
 	}
 
 	if (isNegative == 1)
 		*num *= -1;
-	return 0;
+	return (0);
 }
-
+/**
+ * main - Entry point for app that sum all command line arguments
+ *
+ * parameters:
+ * @argc: number of the command line args
+ * @argv: command line args value
+ *
+ * Return: 0 if succsess
+ */
 int main(int argc, char *argv[])
 {
 	int total = 0, num, i;
@@ -49,7 +66,7 @@ int main(int argc, char *argv[])
 	if (argc < 3)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
 	for (i = 1; i < argc; i++)
@@ -59,13 +76,13 @@ int main(int argc, char *argv[])
 		if (stringToInt(&num, argv[i]) == 1)
 		{
 			printf("Error\n");
-			return 1;
+			return (1);
 		}
 
 		total += num;
 	}
 
 	printf("%i\n", total);
-	return 0;
+	return (0);
 }
 
