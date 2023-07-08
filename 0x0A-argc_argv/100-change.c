49a184b08efd059e47a16ada1feada7cd1601372
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-07 18:19:57 -0700

    4. To infinity and beyond  (automated command)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,36 +1,51 @@
 #include<stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - Entry point for an app that prints the minimum number of coins
+ * to make change for an amount of money and it print Error if input < 0
+ *
+ * parameters:
+ * @argc: number of args values
+ * @argv: array of strings that present the arrguments
+ *
+ * Return: 0 if seccsess , 1 if argc < 2 argv
+ */
 int main(int argc, char *argv[])
 {
-    int list[5] = {25, 10, 5, 2, 1};
-    int arg;
-    int i = 0;
-    int NumberOfCoins = 0;
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
-    arg = atoi(argv[1]);
-    if (arg < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
+	int coin_values[5] = {25, 10, 5, 2, 1};
 
-    for (i = 0; i < 5; i++)
-    {
-        if (arg >= list[i])
-        {
-            NumberOfCoins += arg / list[i];
-            arg = arg % list[i];
-            if (!arg % list[i])
-            {
-                break;
-            }
-        }
-    }
-    printf("%i\n", NumberOfCoins);
-    return 0;
+	int arg, i, coinsNumbr = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	arg = atoi(argv[1]);
+
+	if (arg < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		if (arg >= coin_values[i])
+		{
+			coinsNumbr += arg / coin_values[i];
+			arg = arg % coin_values[i];
+			if (!arg % coin_values[i])
+			{
+				break;
+			}
+		}
+	}
+
+	printf("%i\n", coinsNumbr);
+
+	return (0);
 }
 
