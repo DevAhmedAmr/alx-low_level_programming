a2adb943dbdb31c024500bea8262c2ef38a06ebc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-07 17:16:35 -0700

    4. To infinity and beyond  (automated command)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     int list[5] = {25, 10, 5, 2, 1};
     int total = 0;
     int arg;
-    int i = 0;
+    int i;
     int NumberOfCoins = 0;
 
     /* Check if the number of arguments is correct */
@@ -19,27 +19,21 @@ int main(int argc, char *argv[])
     arg = atoi(argv[1]);
 
     /* Check if the input number is negative */
-    	if (arg < 0)
-	{
-		printf("0\n");
-		return (0);
-	} 
-     	while (total != arg)
-	{
-        if (total + list[i] <= arg)
+    if (arg < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 0; i < 5 && total != arg; i++)
+    {
+        while (total + list[i] <= arg)
         {
             NumberOfCoins++;
             total += list[i];
-            i = 0;
-            continue;
-        }
-
-        i++;
-        if (i > 4)
-        {
-            i = 0;
         }
     }
+
     printf("%i\n", NumberOfCoins);
     return 0;
 }
