78e6e65fa6dff92edd15c034fbbfe55bc31fbd43
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-07 17:24:42 -0700

    4. To infinity and beyond  (automated command)

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,47 @@
 #include<stdio.h>
 #include <stdlib.h>
-
 int main(int argc, char *argv[])
 {
-    int list[5] = {25, 10, 5, 2, 1};
-    int total = 0;
-    int arg;
-    int i;
-    int NumberOfCoins = 0;
+    if (argc == 2)
+    {
 
-    /* Check if the number of arguments is correct */
-    if (argc != 2)
+        int list[5] = {25, 10, 5, 2, 1};
+        int total = 0;
+        int arg = atoi(argv[1]);
+        int i = 0;
+        int NumberOfCoins = 0;
+
+        if (arg < 0)
+        {
+            printf("0\n");
+            return 0;
+        }
+
+        while (total != arg)
+
+        {
+            if (total + list[i] <= arg)
+            {
+                NumberOfCoins++;
+                /*printf("%i\n", list[i]);*/
+                total += list[i];
+                i = 0;
+                continue;
+            }
+
+            i++;
+            if (i > 4)
+            {
+                i = 0;
+            }
+        }
+        printf("%i\n", NumberOfCoins);
+    }
+    else
     {
         printf("Error\n");
         return 1;
     }
-
-    arg = atoi(argv[1]);
-
-    /* Check if the input number is negative */
-    if (arg < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
-
-    for (i = 0; i < 5 && total != arg; i++)
-    {
-        while (total + list[i] <= arg)
-        {
-            NumberOfCoins++;
-            total += list[i];
-        }
-    }
-
-    printf("%i\n", NumberOfCoins);
     return 0;
 }
+
