8831765921b88371595cc096c5a38049ecfe6241
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 10:38:59 -0700

    1. The woman who has no imagination has no wings (automated command)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,10 +4,24 @@ int **alloc_grid(int width, int height)
     int i, j;
     int **arr;
     arr = malloc(height * sizeof(int *));
+
+    if (height < 0 || width < 0)
+    {
+        return NULL;
+    }
+
+    if (*arr == NULL)
+        return NULL;
+
     for (i = 0; i < height; i++)
     {
         arr[i] = malloc(height * sizeof(int *));
+        if (arr[i] == NULL)
+        {
+            return NULL;
+        }
     }
+
     for (
         i = 0; i < height; i++)
     {
