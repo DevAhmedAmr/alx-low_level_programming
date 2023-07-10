56256469f2bfc0c2bf08cd108bc004c3990dc53c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 12:47:58 -0700

    1. The woman who has no imagination has no wings (automated command)

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,21 @@
 #include"main.h"
 #include"stdlib.h"
+/**
+ * free_grid- function that we use to free memory from unwanted 2d arry values
+ * paramters:
+ * @grid: the array that we want to free memory from
+ * @height: the hight of the arry
+ *
+ * Return: void
+ */
+
+/*a good yt vedio that explain how dynamic 2d array in memory works*/
+/*and how to free it: https://www.youtube.com/watch?v=ZLc_OpzND2c */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
