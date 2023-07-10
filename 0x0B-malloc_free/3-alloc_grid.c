fe0303bb4ff295b2417eeb081cff0d35c7e9308e
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 11:17:41 -0700

    1. The woman who has no imagination has no wings (automated command)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,42 +1,38 @@
 #include <stdlib.h>
 int **alloc_grid(int width, int height)
 {
-    int i, j, k;
-    int **arr;
-    arr = malloc(height * sizeof(int *));
+	int i, j, k;
+	int **arr;
 
-    if (height <= 0 || width <= 0)
-    {
-        return NULL;
-    }
+	arr = malloc(height * sizeof(int *));
+	if (height <= 0 || width <= 0 || arr == NULL)
+	{
+		return (NULL);
+	}
 
-    if (arr == NULL)
-        return NULL;
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			/*freeing mem form previous data*/
+			for (k = 0; k < i; k++)
+			{
+				free(arr[k]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
 
-    for (i = 0; i < height; i++)
-    {
-        arr[i] = malloc(width * sizeof(int));
-        if (arr[i] == NULL)
-        {
-            for (k = 0; k <= i; k++)
-            {
-                free(arr[k]);
-            }
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
 
-            free(arr);
-            return NULL;
-        }
-    }
-
-    /*                                                     */
-    for (
-        i = 0; i < height; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
-
-    return arr;
+	return (arr);
 }
+
