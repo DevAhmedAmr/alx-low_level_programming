09a9a529a0b34707639cac8062ce0a01faab3d6b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 03:15:55 -0700

    0. Float like a butterfly, sting like a bee (automated command)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,15 +6,18 @@ char *create_array(unsigned int size, char c)
 	char *arry;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 	
 	arry = malloc(sizeof(char) * size);
+
+	if (arry == NULL)
+		reutrn (NULL);
 
 	for (i = 0; i < size; i++)
 	{
 		arry[i] = c;
 	}
 
-	return arry;
+	return (arry);
 }
 
