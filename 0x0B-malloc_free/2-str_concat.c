21af1370e8eeae5e57a9f46a8408e7165e419fd6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 09:48:47 -0700

    1. The woman who has no imagination has no wings (automated command)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,44 +15,52 @@ int _strlen(char *s)
 	return (len);
 }
 
+/**
+ * str_concat - function that append the second strin to the first string
+ *
+ * parameter:
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: first String + second string
+ */
 char *str_concat(char *s1, char *s2)
 {
 	int i, j, len;
 	char *str;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 
-	if(s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
-    len = _strlen(s1) + _strlen(s2) + 1;
+	len = _strlen(s1) + _strlen(s2) + 1;
 
-    str = malloc(len * sizeof(char));
+	str = malloc(len * sizeof(char));
 
-    if(str==NULL)
-	    return NULL;
+	if (str == NULL)
+		return (NULL);
 
-    for (i = 0; i < _strlen(s1); i++)
-    {
-        if (s1[i] != '\0')
-        {
-            str[i] = s1[i];
-        }
-    }
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		if (s1[i] != '\0')
+		{
+			str[i] = s1[i];
+		}
+	}
 
-    i = 0;
+	i = 0;
 
-    for (j = _strlen(s1); j < len; j++)
-    {
+	for (j = _strlen(s1); j < len; j++)
+	{
+		if (s2[i] != '\0')
+		{
+			str[j] = s2[i];
+			i++;
+		}
+	}
 
-        if (s2[i] != '\0')
-        {
-            str[j] = s2[i];
-
-            i++;
-        }
-    }
-
-    return str;
+	return (str);
 }
+
