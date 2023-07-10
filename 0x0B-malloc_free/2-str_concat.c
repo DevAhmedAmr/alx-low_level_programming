4e6e0e4dc2a0945107ce7a71dadb4d83b1b32da1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-10 09:22:32 -0700

    1. The woman who has no imagination has no wings (automated command)

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+/**
+ * _strlen -  a function that returns the length of a string.
+ *
+ * @s: type str
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	len++;
+	return (len);
+}
+
+char *str_concat(char *s1, char *s2)
+{
+    int i, j;
+
+    int len = _strlen(s1) + _strlen(s2) + 1;
+
+    char *str = malloc(len * sizeof(char));
+
+    for (i = 0; i < _strlen(s1); i++)
+    {
+        if (s1[i] != '\0')
+        {
+            str[i] = s1[i];
+        }
+    }
+
+    i = 0;
+    for (j = _strlen(s1); j < len; j++)
+    {
+
+        if (s2[i] != '\0')
+        {
+            str[j] = s2[i];
+
+            i++;
+        }
+    }
+
+    return str;
+}
