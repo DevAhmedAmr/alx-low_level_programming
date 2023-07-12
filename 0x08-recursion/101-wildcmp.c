6ecef9cbb932a54b021c809ba2659e64f3dee8ee
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-12 15:03:31 -0700

    task 8. Inception. Now, before you bother telling me it's impossible (automated command)

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,13 +25,26 @@ int wildcmpHelper(char *s1, char *s2, int seenAstar)
         s1++;
         s2++;
     }
-    if ((*s1 == '\0' && *s2 != '\0') || (*s2 == '\0' && *s1 != '\0'))
+      if (*s1 == '\0' && *s2 != '\0')
     {
         if (*s2 == '*')
         {
             s2++;
         }
         else
+        {
+            return 0;
+        }
+    }
+    if (*s1 != '\0' && *s2 == '\0')
+    {
+        s2 -= 1;
+        if (*s1 != *s2 && *s1 != '\0')
+        {
+            s1++;
+            return wildcmpHelper(s1, s2, seenAstar);
+        }
+        if (*s1 != *s2)
         {
             return 0;
         }
