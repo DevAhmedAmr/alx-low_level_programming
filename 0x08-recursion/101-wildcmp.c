555cb1275e0d3f864076e0051e73dd36c5a020e9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-12 13:00:56 -0700

    task 8. Inception. Now, before you bother telling me it's impossible (automated command)

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,68 @@
+int wildcmpHelper(char *s1, char *s2, int seenAstar)
+{
+
+    if (*s2 == '*')
+    {
+
+        s2++;
+        seenAstar = 1;
+        return wildcmpHelper(s1, s2, seenAstar);
+    }
+    if (*s1 != *s2 && *s1 != '\0')
+    {
+
+        if (seenAstar == 1)
+        {
+
+            s1++;
+        }
+        else
+            return 0;
+    }
+    if (*s1 == *s2 && *s1 != '\0' && *s2 != '0')
+    {
+
+        s1++;
+        s2++;
+    }
+    if ((*s1 == '\0' && *s2 != '\0') || (*s2 == '\0' && *s1 != '\0'))
+    {
+        if (*s2 == '*')
+        {
+            s2++;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    if (*s1 == '\0' && *s2 == '\0')
+    {
+
+        return 1;
+    }
+
+    return wildcmpHelper(s1, s2, seenAstar);
+
+    return 1;
+}
+
+int wildcmp(char *s1, char *s2)
+{
+    int seenAstar = 0;
+    if (*s2 == '*')
+    {
+        s2++;
+        seenAstar = 1;
+        return wildcmp(s1, s2);
+    }
+
+    if (*s1 != *s2 && *s1 != '\0')
+    {
+        s1++;
+        return wildcmp(s1, s2);
+    }
+
+    return wildcmpHelper(s1, s2, seenAstar);
+}
+
