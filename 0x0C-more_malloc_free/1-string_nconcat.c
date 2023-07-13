ec59c9d096758691686e1524a385f14e359d0778
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-13 13:27:11 -0700

    0. Trust no one  (automated command)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,12 +4,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     unsigned int s1Len = 0, i, j, k = 0;
     char *newSentance;
 
+    if(*s1 == NULL)
+	    *s1 = "";
+    if (s2 == NULL)
+	    s2 = "";
+
     /*get the len of s1*/
     while (s1[s1Len] != '\0')
         s1Len++;
 
+    if (s2len <= n)
+	    return
+
     /*allocated the memory*/
     newSentance = malloc((s1Len + n + 1) * sizeof(char));
+
+    if (newSentance == NULL)
+	    return (NULL);
 
     /*add s1*/
 
