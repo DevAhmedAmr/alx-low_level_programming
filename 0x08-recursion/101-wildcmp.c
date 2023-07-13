ae879aa9fda20e36616706413845432fbcb88df9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-12 17:52:15 -0700

    removing casting for malloc  (automated command)

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -82,3 +82,34 @@ int wildcmp(char *s1, char *s2)
 	return (wildcmpHelper(s1, s2, seenAstar));
 }
 
+/*
+ * function Explanation
+ *
+ * The function initializes the seenAstar flag and 
+ * skips any leading * characters in s2. It moves the pointer s1 to 
+ * the position of the first character match in the two strings.
+ *
+ * The function enters a loop to compare the strings one
+ * character at a time.
+ *
+ * If a * character is encountered in s2, the pointer s2 moves to
+ * the next character, and the seenAstar flag is set tp true.
+ *
+ * If the characters do not match and s1 has not reached the end, the pointer
+ * s1 moves to the next character if the seenAstar flag is set. Otherwise,
+ * the function retuns 0 (no match).
+ *
+ * If the characters match and are not the end of the strings,
+ * both pointers s1 and s2 move to the next characters.
+ *
+ * If the end of s1 is reached but not the end of s2, the function checks
+ * if the remaining characters in s2 are * characters and skips them. If not,
+ *  the function returns 0 (no match).
+ *
+ * If the end of s2 is reached but not the end of s1,
+ * the function moves the pointer s1 to the position of the
+ * last character match in the two strings. If no match is found,
+ * the function returns 0 (no match).
+ *
+ * If the end of both strings is reached, the function returns 1 (match).
+ */
