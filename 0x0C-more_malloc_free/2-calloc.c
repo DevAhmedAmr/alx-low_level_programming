e71a73f7c4371e0dc05753cfa51013d812fc392a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-13 17:31:10 -0700

    2. _calloc  (automated command)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,12 +3,14 @@ void *_calloc(size_t nmemb, size_t size)
 {
 	size_t i;
 
-	char *p = malloc(nmemb * size);
-
-	if (p == NULL || size == 0 || nmemb == 0)
+	char *p;
+	if (size == 0 || nmemb == 0)	
 		return NULL;
+
+	p= malloc(nmemb * size);
 
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
+
 	return p;
 }
