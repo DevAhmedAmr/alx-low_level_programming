25add190bc2637a0c5f1ad10a36aa30d84b38efe
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-13 19:20:52 -0700

    4. _realloc  (automated command)

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    void *ptrCpy;
+    unsigned int i;
+
+    if (new_size == old_size)
+        return ptr;
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return NULL;
+    }
+
+    if (ptr == NULL)
+    {
+        ptrCpy = malloc(new_size * sizeof(char));
+
+        if (ptrCpy == NULL)
+            return NULL;
+
+        return ptrCpy;
+    };
+
+    ptrCpy = malloc(new_size * sizeof(char));
+
+    if (ptrCpy == NULL)
+        return NULL;
+
+    for (i = 0; i < old_size; i++)
+        ((char *)ptrCpy)[i] = ((char *)ptr)[i];
+
+    free(ptr);
+    return ptrCpy;
+}
