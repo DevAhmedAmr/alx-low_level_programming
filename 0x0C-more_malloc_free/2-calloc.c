152bd5f618425b286e1ceec9e48a0a82c0026561
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-13 17:50:53 -0700

    2. _calloc  (automated command)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,13 @@
 #include<stdlib.h>
+/**
+ * _calloc - my own calloc function in c that allcoate memroy and set it to 0
+ *
+ * parameters:
+ * @nmemb: number of elements to be allocated
+ * @size: size of the element (if int 4 bytes , char 1 byte, etc.. )
+ *
+ * Return: a pointer to the  address of the allocated memory
+ */
 
 void *_calloc(size_t nmemb, size_t size)
 {
@@ -6,12 +15,12 @@ void *_calloc(size_t nmemb, size_t size)
 
 	void *p;
 
-	if (size == 0 || nmemb == 0)	
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 
 	p = malloc(nmemb * size);
 
-	if(p == NULL)
+	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
