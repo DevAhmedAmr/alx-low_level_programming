f80166d2ba80c473a55cc04e832365ceb74ce827
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-13 18:16:43 -0700

    2. _calloc  (automated command)

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,10 @@
 int *array_range(int min, int max)
 {
+	int i, arr_index;
+
     int *arr = malloc((max - min + 1) * sizeof(int));
-    for (int i = min, arr_index = 0; i <= max; i++, arr_index++)
+
+    for ( i = min, arr_index = 0; i <= max; i++, arr_index++)
     {
         arr[arr_index] = i;
     }
