8140703366fd265e13a5328a46ff4bd2421602c9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-14 19:30:21 -0700

    5. We must accept finite disappointment, but never lose infinite hope  (automated command)

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -85,8 +85,8 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		/*printf("Error\n");*/
-		/*exit(98);*/
+		printf("Error\n");
+		exit(98);
 	}
 	num1 = malloc(strlen(argv[1]) * sizeof(char) + 1);
 	num2 = malloc(strlen(argv[2]) * sizeof(char) + 1);
@@ -136,8 +136,8 @@ int main(int argc, char **argv)
 
 				if (subNum1 > 9 || subNum2 > 9)
 				{
-					printf("Error\n");
-					exit(98);
+					/*printf("Error\n");*/
+					/*exit(98);*/
 				}
 
 				mul = subNum2 * subNum1 + carry;
