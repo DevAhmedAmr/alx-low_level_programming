1a6809cbc8385ae249a570647582cfe2457be640
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-15 20:01:26 -0700

    fixing some betty (automated command)

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/**
- * _strcpy - my custom implementation for strcpy function in c
- *
- * @dest: destination
- * @src: src to be copied
- *
- * Return: a pointer for destination after copying src to it
- */
-char *_strcpy(char *dest, char *src)
-
-{
-	unsigned int i = 0;
-
-	for (; i <= strlen(src); i++)
-
-		dest[i] = src[i];
-	
-	return (dest);
-}
 
 /**
  * reverseString - function that reverse a string by swaping the string chars
@@ -31,9 +12,11 @@ void reverseString(char *str)
 {
 	int i;
 	int length = strlen(str);
+
 	for (i = 0; i < length / 2; i++)
 	{
 		char temp = str[i];
+
 		str[i] = str[length - 1 - i];
 		str[length - 1 - i] = temp;
 	}
@@ -68,14 +51,13 @@ void ifninty_Add(char *num1, char *num2, char *result)
 	reverseString(result);
 }
 
-
 /**
- * additional_Zeros_remover - function that removes the leading zeros
- * in the result if the input is 00 ,000 ,0000 ,etc
+ * additional_Zeros_remover - function that removes additional
+ * leading zeros in the result if the input is zeros 0, 00, 000, etc
+ * @parameters:
+ * @numb: the number to be modified in string form
  *
- * @numb: the result that will be modifyed
- *
- * Return:void
+ * Return: void
  */
 void additional_Zeros_remover(char *numb)
 {
@@ -83,6 +65,7 @@ void additional_Zeros_remover(char *numb)
 	int i;
 	int len = strlen(numb);
 	int isZero = 0;
+
 	for (i = 0; i < len; i++)
 	{
 		if (numb[i] != '0')
@@ -93,84 +76,46 @@ void additional_Zeros_remover(char *numb)
 	}
 	if (isZero == 0)
 	{
-		_strcpy(numb, "0");
+
+		strcpy(numb, "0");
 	}
 }
 /**
- * main - Entry point to program the multiablicate infinty numbers of intigers
+ * multiplicate - function that multiplicate 2 infinite numbers
  *
- * @argc: number of arguments
- * @argv: arguments values
+ * @parameters:
+ * @num1: first number
+ * @num2: second number
  *
- * Return: 1 on succsess
+ * Return: 0 on successes , if fails returns 1
  */
-int main(int argc, char **argv)
+
+int multiplicate(char *num1, char *num2)
 {
-	char *num1;
-	char *num2;
-
 	int mul, len1, len2, carry = 0, buffer_count = 0, i, k, j;
-	int needCarry = 0;
-	int zerosCountAdd = 0;
-	char *finalSum;
-	char *buffer;
-	char *tmp;
-	int subNum1, subNum2;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	num1 = malloc(strlen(argv[1]) * sizeof(char) + 1);
-	num2 = malloc(strlen(argv[2]) * sizeof(char) + 1);
-
-	if (num1 == NULL || num2 == NULL)
-	{
-		/*fprintf(stderr, "Memory allocation failed!\n");*/
-		if (num1 != NULL)
-			free(num1);
-
-		if (num2 != NULL)
-			free(num2);
-
-		return 1;
-	}
-
-	strcpy(num1, argv[1]);
-	strcpy(num2, argv[2]);
+	int needCarry = 0, zerosCountAdd = 0, subNum1, subNum2;
+	char *finalSum, *buffer, *tmp;
 
 	len1 = strlen(num1);
 	len2 = strlen(num2);
-
 	finalSum = malloc(sizeof(char) * (len1 + len2 + 1));
 	buffer = malloc(sizeof(char) * (len1 + len2 + 1));
 	tmp = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (finalSum == NULL || buffer == NULL || tmp == NULL)
 	{
-		/*fprintf(stderr, "Memory allocation failed!\n");*/
-		if (finalSum != NULL)
-			free(finalSum);
-
-		if (buffer != NULL)
-			free(buffer);
-
-		if (tmp != NULL)
-			free(tmp);
-
+		fprintf(stderr, "Memory allocation failed!\n");
 		free(num1);
 		free(num2);
-
-		return 1;
+		return (1);
 	}
-
 	finalSum[0] = '0';
 	finalSum[1] = '\0';
 
 	for (i = len2 - 1; i >= 0; i--)
 	{
 		carry = 0;
+
 		for (k = 0; k < zerosCountAdd; k++)
 		{
 			buffer[buffer_count] = 48;
@@ -187,13 +132,6 @@ int main(int argc, char **argv)
 				if (subNum1 > 9 || subNum2 > 9)
 				{
 					printf("Error\n");
-
-					free(finalSum);
-					free(buffer);
-					free(tmp);
-					free(num1);
-					free(num2);
-
 					exit(98);
 				}
 
@@ -223,7 +161,6 @@ int main(int argc, char **argv)
 		strcpy(finalSum, tmp);
 		buffer_count = 0;
 	}
-	additional_Zeros_remover(finalSum);
 
 	printf("%s\n", finalSum);
 
@@ -232,8 +169,28 @@ int main(int argc, char **argv)
 	free(tmp);
 	free(num1);
 	free(num2);
-
-	return 0;
+	return (0);
 }
 
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of 2 numbers in a string form
+ * Return: 0 on success
+ */
 
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (multiplicate(argv[1], argv[2]) == 1)
+	{
+		return (1);
+	}
+
+	return (0);
+}
