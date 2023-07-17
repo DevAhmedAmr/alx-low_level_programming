8d92fdaf57878b6b91fe052ab2bbb8e6d6c240ba
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-17 15:41:32 -0700

    fixed a if the input is null for task 1 (automated command)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include"dog.h"
+#include<stdio.h>
 /**
  * init_dog - function that takes apointer to struct and sets its values
  *
@@ -11,8 +12,12 @@
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+
 {
+	if (d != NULL)
+	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
 }
