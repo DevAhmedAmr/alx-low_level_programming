8823130da790138a1443ed215550f4fefaf271fa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-17 19:16:39 -0700

    first commit (automated command)

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,23 @@
 #include"dog.h"
 #include<stdlib.h>
+/**
+ * free_dog - function that free dog_t struct from the memory
+ *
+ * parameters:
+ * @d:struct to be freed from the memory
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
-	if (d != NULL){
+	if (d != NULL)
+	{
 
-	if(d->name != NULL)
+	if (d->name != NULL)
 		free(d->name);
 
-	if(d->owner != NULL)
+	if (d->owner != NULL)
 		free(d->owner);
-
-        free(d);
+	free(d);
 	}
 }
