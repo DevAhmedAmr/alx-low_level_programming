01ebaa233d669d67e99c82a79bfabce50adbcc96
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-17 18:22:12 -0700

    first commit (automated command)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,13 +2,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+/**
+ * new_dog -  function that creates a pointer to a  new dog.
+ *
+ * @parameters:
+ * @name: name of the new dogo
+ * @age: age of the new dogo
+ * @owner: name of the owner of the new dogo
+ * Return: a pointer to the new dogo
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	my_dog->name = malloc((strlen(name) + 1) * sizeof(char));
@@ -16,7 +25,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
-		return NULL;
+		return (NULL);
 	}
 	strcpy(my_dog->name, name);
 
@@ -27,9 +36,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		free(my_dog->name);
 		free(my_dog);
-		return NULL;
+		return (NULL);
 	}
 	strcpy(my_dog->owner, owner);
 
-	return my_dog;
+	return (my_dog);
 }
