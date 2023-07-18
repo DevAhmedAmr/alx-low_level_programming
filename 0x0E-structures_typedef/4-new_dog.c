6efc1347ce7f12cf16f37d508328fd6d0adcd34a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-17 18:14:01 -0700

    first commit (automated command)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,34 @@
 #include"dog.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
-	if (my_dog == NULL)
-		return NULL;
 
-	my_dog->name = name;
+	if (my_dog == NULL)
+	{
+		return NULL;
+	}
+
+	my_dog->name = malloc(strlen(name) + 1 * sizeof(char));
+
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return NULL;
+	}
+	strcpy(my_dog->name, name);
+
 	my_dog->age = age;
-	my_dog->owner = owner;
+
+	my_dog->owner = malloc(strlen(owner) + 1 * sizeof(char));
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		return NULL;
+	}
+	strcpy(my_dog->owner, owner);
+
 	return my_dog;
 }
