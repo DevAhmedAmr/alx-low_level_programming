928d57934067feb5bc1ca32941bdfb470d7f87bc
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 16:56:08 -0700

    fixed betty for task 3 (automated command)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include <stddef.h>
 #include "function_pointers.h"
+#include <stddef.h>
+
 /**
- * print_name - function that takes a pointer to an another fuction and
- * excute it
- *
- * parameters:
- * @name: name to be printed
- * @f: pointer to function to be excuted
+ * print_name - eazea
+ * @name: aeza
+ * @f: eza
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-(*f)(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }
