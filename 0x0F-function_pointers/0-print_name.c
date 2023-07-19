903ddc19052ac31f7dedcfd368483f9d716f0333
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 15:27:14 -0700

    fixed betty in 0-main.c (automated command)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,14 @@
 #include <stddef.h>
+/**
+ * print_name - function that takes a pointer to an another fuction and
+ * excute it
+ *
+ * parameters:
+ * @name: name to be printed
+ * @f: pointer to function to be excuted
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
