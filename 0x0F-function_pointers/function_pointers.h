ae6ce688818c7b299a5105fae1fa563a8e2f181c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 16:48:14 -0700

    fixed betty for task 3 (automated command)

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,12 +2,21 @@
 #define FUNCTION_POINTER_H
 
 #include <stddef.h>
+
 #include <stdio.h>
+
 #include <stdlib.h>
+
 int _putchar(char c);
+
 void print_name(char *name, void (*f)(char *));
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
 int int_index(int *array, int size, int (*cmp)(int));
+
 void print_name_uppercase(char *name);
+
 void print_name_as_is(char *name);
+
 #endif
