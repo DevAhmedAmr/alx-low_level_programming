b4e8b7eca0aabf5cfdb9fb4653def5ed88eb1815
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 15:17:11 -0700

    fixed betty in 0-main.c (automated command)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,7 +10,6 @@
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
-{
-	if (f != NULL && name != NULL)
+{         if (f != NULL && name != NULL)
 		f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - function that searches for an integer in array
+ * 
+ * parameters:
+ * @size:
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
