bd8c0446a9dda6461b8be3d0155367db50a7db8a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 15:37:57 -0700

    fixed betty for task 2  (automated command)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,27 +1,32 @@
 #include "function_pointers.h"
 
 /**
- * int_index - function that searches for an integer in array
- * 
+ * int_index - function that searches for the first integer in array to meet
+ * a requirement
+ *
  * parameters:
- * @size:
+ * @size: size of the arr
+ * @array: the array to be searched
+ * @cmp: pointer to a function that searches
+ * with the poistive or absolute value
+ *
+ * Return: index of the element or -1
+ */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if(cmp == NULL || array == NULL)
+	if (cmp == NULL || array == NULL)
 		return (-1);
 
 	if (size <= 0)
 		return (-1);
 
-	for ( i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if ((*cmp)(array[i]) == 1)
-		{
-			return i;
-		}
+			return (i);
 	}
 	return (-1);
 }
