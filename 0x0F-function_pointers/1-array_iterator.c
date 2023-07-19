29e2817c6af588bb585595e24a8e3e67ef11162b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 14:05:03 -0700

    fixed some issues in task 1 (automated command)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL && size > 0 && array != NULL)
+	if (action != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			(*action)(array[i]);
