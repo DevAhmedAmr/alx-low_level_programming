92f006838d81ec65b11fdf67c410177417b489f0
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 02:55:04 -0700

    first commit (automated command)

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include"calc.h"
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "/") != 0 &&
+		strcmp(argv[2], "-") != 0 && strcmp(argv[2], "%") != 0 &&
+		strcmp(argv[2], "*") != 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(num1, num2));
+
+	return 0;
+}
