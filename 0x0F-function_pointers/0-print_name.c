a04e55a7b23c9e2a6f18c513e45415b9bb163d91
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 14:21:19 -0700

    fixed some issues in task 1 (automated command)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,20 +10,9 @@
  *
  * Return: void
  */
-int _strcmp(const char* str1, const char* str2)
-{
-    while (*str1 && (*str1 == *str2))
-    {
-        str1++;
-        str2++;
-    }
-
-    return *(const unsigned char*)str1 - *(const unsigned char*)str2;
-}
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL && name != NULL && _strcmp("",name) == 0)
+	if (f != NULL && name != NULL )
 		f(name);
 }
 
