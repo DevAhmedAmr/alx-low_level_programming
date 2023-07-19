bd38a46282e6697d3c149513af76036c28eb7166
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 14:02:36 -0700

    fixed some issues in task 1 (automated command)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,23 @@
 #include <stddef.h>
+/**
+ * array_iterator - function that iterate an array based on
+ * the ("action" pointer to function)
+ *
+ * parameters:
+ * @array: array to be iterated
+ * @size: size of the array
+ * @action: pointer to function that will perfrom action on the array element
+ *
+ * Return: void
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-	(*action)(array[i]);
-	
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
 }
