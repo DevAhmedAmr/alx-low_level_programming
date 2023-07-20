0bc430e8e3f1b1f8ac9d46e3bc2c6f5d2faf425d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-20 06:38:55 -0700

    fixed betty (automated command)

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,29 @@
 #include "variadic_functions.h"
+/**
+ * sum_them_all - function that sum all the parameters
+ *
+ * parameters:
+ * @n: number of paramters
+ *
+ * Return: sum of all the parameters
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int sum = 0, i;
+
 	va_list args;
 
 	if (n == 0)
-		return 0;
+		return (0);
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		int num = va_arg(args, int);
+
 		sum += num;
 	}
 	va_end(args);
-	return sum;
+	return (sum);
 }
