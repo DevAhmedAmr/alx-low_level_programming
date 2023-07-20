f76197be71755000d0a0f7d6f19c8c0aa4b0b7fa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 18:13:05 -0700

    advanced task 4 (automated command)

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,35 +11,31 @@
  */
 int main(int argc, char *argv[])
 {
-        int i;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                exit(1);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-        }
+	int num_bytes = atoi(argv[1]);
 
-        int num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-        if (num_bytes < 0)
-        {
-                printf("Error\n");
-                exit(2);
+	unsigned char *function_ptr = (char *)main;
 
-        }
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx", function_ptr[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 
-        char *function_ptr = (char *)main;
-
-        for (i = 0; i < num_bytes; i++)
-        {
-		printf("%02hhx ", function_ptr[i]);
-
-                if (i < num_bytes - 1)
-                        printf(" ");
-                else
-                        printf("\n");
-        }
-
-        return (0);
+	return 0;
 }
