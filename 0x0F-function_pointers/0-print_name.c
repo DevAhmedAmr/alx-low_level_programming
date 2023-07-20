586ad59fb97cc01b2640c9b5552527b442b89246
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-19 17:10:24 -0700

    fixed task 0 check 5 (the problem was the name of the file header was not correct in ifndef ... ) (automated command)

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,16 @@
-#include "function_pointers.h"
 #include <stddef.h>
-
+#include "function_pointers.h"
 /**
- * print_name - eazea
- * @name: aeza
- * @f: eza
+ * print_name - function that takes a pointer to an another fuction and
+ * excute it
+ *
+ * parameters:
+ * @name: name to be printed
+ * @f: pointer to function to be excuted
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 		(*f)(name);
 }
+~   
