ad0e5573f829e48b838a15c745cf6e7988faf527
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-20 19:52:26 -0700

    first commit (automated command)

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,22 +1,46 @@
 #include "variadic_functions.h"
+/**
+ * print_char - function that prints a char
+ *
+ * @args: char to be printed
+ */
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
+
+/**
+ * print_int - function that prints intigers
+ *
+ * @args: integer to be printed
+ */
 
 void print_int(va_list args)
 {
 	printf("%i", va_arg(args, int));
 }
 
+/**
+ * print_float - function that prints a float
+ *
+ * @args: float to be printed
+ */
+
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 
+/**
+ * print_string - function that prints a string
+ *
+ * @args: string to be printed
+ */
+
 void print_string(va_list args)
 {
 	char *string = va_arg(args, char *);
+
 	if (string == NULL)
 	{
 		printf("(nil)");
@@ -24,18 +48,17 @@ void print_string(va_list args)
 	}
 	printf("%s", string);
 }
-
+/**
+ * print_all - that prints the input based on the specifier
+ * @format: the specifier for the input to be printed
+ */
 void print_all(const char *const format, ...)
 {
 	va_list args;
 	unsigned int format_index, formater_index;
 	char *separator = "";
 
-	struct format_ops
-	{
-		char *specifier;
-		void (*func)(va_list);
-	} formater[] = {
+	format_ops formater[] = {
 		{"c", print_char},
 		{"i", print_int},
 		{"f", print_float},
@@ -60,7 +83,6 @@ void print_all(const char *const format, ...)
 		}
 		format_index++;
 	}
-
 	printf("\n");
 	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,4 +14,15 @@ int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct format_ops - struct to hold format specifier
+ * and the function that will print it
+ * @specifier: format specifier
+ * @func: function that will print
+ */
+typedef struct format_ops
+{
+    char *specifier;
+    void (*func)(va_list);
+} format_ops;
 #endif /* VARIADIC_FUNCTIONS_H */
