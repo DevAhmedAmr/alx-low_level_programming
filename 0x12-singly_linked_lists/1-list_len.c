fefab4d74025ff9c245d0d13a0bd880eb6b8f07e
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-28 00:04:42 +0300

    added documentation (automated command)

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * list_len - function return the len of a linked list
+ * @h: first node on the linked list
+ * Return:the len of a linked list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
@@ -7,5 +12,5 @@ size_t list_len(const list_t *h)
 		len++;
 		h = h->next;
 	}
-	return len;
+	return (count);
 }
