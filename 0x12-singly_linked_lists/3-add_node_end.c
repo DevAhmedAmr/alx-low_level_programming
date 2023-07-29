4069b75a9180fcadbdabf6dc08559785b5c27721
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-29 13:01:04 +0300

    (automated command)

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,40 +12,38 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	char *dup = malloc(sizeof(char) * strlen(str) + 1);
-
-	if (new == NULL || dup == NULL)
+	int len = strlen(str);
+	char *strCpy = malloc(len + 1 * sizeof(char));
+	if (new == NULL || strCpy == NULL)
 	{
 		if (new != NULL)
 			free(new);
 
-		if (dup != NULL)
+		if (strCpy != NULL)
 			free(new);
 
 		return (NULL);
 	}
 
-	dup = strcpy(dup, str);
-
-	new->str = dup;
-
-	new->len = strlen(str);
-
-	new->next = NULL;
+	new->len = len;
+	strCpy = strcpy(strCpy, str);
+	new->str = strCpy;
 
 	if (*head == NULL)
+	{
+		new->next = NULL;
 		*head = new;
-
+	}
 	else
 	{
-		list_t *last_node = *head;
 
-		while (last_node->next != NULL)
-		{
-			last_node = last_node->next;
-		}
-		last_node->next = new;
+		list_t *tmp_node = *head;
+
+		while (tmp_node->next != NULL)
+			tmp_node = tmp_node->next;
+		new->next = NULL;
+
+		tmp_node->next = new;
 	}
-
 	return (new);
 }
