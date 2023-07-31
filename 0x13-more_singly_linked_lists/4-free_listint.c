972a8e73067c9cadec1f639cd9bb9ad6d60c3366
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 22:30:47 +0300

    task 4. Free list (automated command)

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 
 /**
+ * free_listint -  a function that frees a listint_t list.
+ *
+ * @param:
+ * @head: ptr to the list to be freed from the memory
  *
  */
 void free_listint(listint_t *head)
 {
 	listint_t *next;
+
 	while (head != NULL)
 	{
 		next = head->next;
