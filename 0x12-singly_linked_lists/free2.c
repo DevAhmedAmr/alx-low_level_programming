d289147697f0e51c4dd745901d5b4eeec5bfe9b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 04:53:04 -0700

    first commit (automated command)

---
+++ 0x12-singly_linked_lists/free2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_list - function that Frees a linked list
+ *
+ * @head: A pointer to the head of the linked list
+ *
+ * Return: void
+ *
+ */
+
+void free_list(list_t *head)
+{
+	/*https://www.youtube.com/watch?v=ld8xjJ3j-eY*/
+
+	list_t *next = head->next;
+
+	free(head->str);
+
+	free(head);
+
+	head = next;
+
+	if (next != NULL)
+	{
+		free_list(next);
+	}
+}
+
