6dfa849f0c10bf563b87d51cb8d1ea3792da57da
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 21:40:18 +0300

    task 2. Add node  (automated command)

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * add_nodeint - unction that adds a new node at the beginning of a listint_t list.
+ * add_nodeint - unction that adds a new node at
+ * the beginning of a listint_t list.
  *
  * @param:
  * @head: pointer to the address if the head of the linked list
