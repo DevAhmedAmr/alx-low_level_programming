acee751cdf2180870c8fb3c694c03afd0739d109
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 23:47:53 +0300

    test (automated command)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,13 +6,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode = malloc(sizeof(listint_t));
 	unsigned int index = 0;
 
-	if (newNode == NULL || head == NULL)
+	if (newNode == NULL)
 		return NULL;
 
 	curr = *head;
 
+	if (curr == NULL && index == 0)
+	{
+		newNode->n = n;
+
+		newNode->next = *head;
+		*head = newNode;
+		return newNode;
+	}
+
 	while (curr != NULL)
 	{
+
 		int isLastIndex = (idx == 1 + index && curr->next == NULL);
 
 		if (index + 1 == idx)
@@ -22,14 +32,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 
 			newNode->n = n;
-			if (idx > 0)
-
-				before->next = newNode;
-
-			else
+			if (idx <= 0)
 			{
+
 				newNode->next = *head;
 				*head = newNode;
+			}
+			else
+			{
+				before->next = newNode;
 			}
 
 			if (isLastIndex)
---
+++ 0x13-more_singly_linked_lists/9-main.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    printf("-----------------\n");
+    insert_nodeint_at_index(&head, 5, 4096);
+    print_listint(head);
+    free_listint2(&head);
+    printf("-----------------\n");
+
+    head2 = NULL;
+
+    insert_nodeint_at_index(&head2, 0, 409006);
+    add_nodeint_end(&head2, 0);
+    add_nodeint_end(&head2, 1);
+    print_listint(head2);
+
+    return (0);
+}
