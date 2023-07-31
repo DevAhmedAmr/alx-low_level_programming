e60f313a4cb627d62ba8f1887af9003386746e1f
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 22:53:00 +0300

    adding some edge cases for task 9  (automated command)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,6 +5,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *before;
 	listint_t *newNode = malloc(sizeof(listint_t));
 
+	if (newNode == NULL || head == NULL)
+		return NULL;
+
 	unsigned int index = 0;
 	curr = *head;
 
