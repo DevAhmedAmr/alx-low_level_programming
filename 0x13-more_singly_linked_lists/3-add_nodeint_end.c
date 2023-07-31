921cc6801ebbd8a0326965b258a1749c08c71508
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 22:18:05 +0300

    task 2. Add node  (automated command)

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,20 +1,35 @@
 #include "lists.h"
+/**
+ * add_nodeint_end - Write a function that adds a new node
+ * at the end of a listint_t list.
+ *
+ * @param:
+ * @head: ptr to the linked list
+ * @n: number to be addes to the linked list
+ *
+ * Return: ptr to the new added element
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
 	newNode->n = n;
 	newNode->next = NULL;
 
 	if (*head == NULL)
-	{
 		*head = newNode;
-	}
+
 	else
 	{
-		listint_t *tmp = *head;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = newNode;
+		listint_t *curr = *head;
+
+		while (curr->next != NULL)
+			curr = curr->next;
+
+		curr->next = newNode;
 	}
-	return newNode;
+	return (newNode);
 }
