a8dfbd30ea44a058f9c476887ce6a0ad2786f89e
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 21:35:12 +0300

    test (automated command)

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,19 @@
 #include "lists.h"
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	if (*head == NULL)
+	{
+		return NULL;
+	}
+
 	listint_t *newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return NULL;
+	}
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
-	return newNode;
+	return *head;
 }
