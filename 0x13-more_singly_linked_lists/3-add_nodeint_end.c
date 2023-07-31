87be9dc1d2b4c3749961e6e55d7e444117f9ee53
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 22:03:52 +0300

    task 2. Add node  (automated command)

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+	}
+	else
+	{
+		listint_t *tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newNode;
+	}
+}
---
+++ 0x13-more_singly_linked_lists/3-main.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+
+	head2 = NULL;
+	add_nodeint_end(&head2, 5555);
+
+	print_listint(head2);
+
+	return (0);
+}
