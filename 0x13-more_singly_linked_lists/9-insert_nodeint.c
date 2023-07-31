53613a05c67617968292ea6bb3916c2892741fb5
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 09:43:19 -0700

    first commit (automated command)

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *curr;
+	listint_t *before;
+	listint_t *newNode = malloc(sizeof(listint_t));
+
+	unsigned int index = 0;
+	curr = *head;
+
+	while (curr != NULL)
+	{
+		int isLastIndex = (idx == 1 + index && curr->next == NULL);
+
+		if (index + 1 == idx)
+			before = curr;
+
+		if (idx == index || isLastIndex)
+		{
+
+			newNode->data = n;
+			if (idx > 0)
+
+				before->next = newNode;
+
+			else
+			{
+				newNode->next = *head;
+				*head = newNode;
+			}
+
+			if (isLastIndex)
+
+				newNode->next = NULL;
+			else
+				newNode->next = curr;
+
+			break;
+		}
+		else
+			index++;
+		curr = curr->next;
+	}
+	return newNode;
+}
+
---
+++ 0x13-more_singly_linked_lists/main.h
@@ -0,0 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+#endif
