af707b64b5387589868e929718866d83809fb4a8
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 21:39:16 +0300

    task 2. Add node  (automated command)

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,15 +1,22 @@
 #include "lists.h"
+/**
+ * add_nodeint - unction that adds a new node at the beginning of a listint_t list.
+ *
+ * @param:
+ * @head: pointer to the address if the head of the linked list
+ * @n: the value of n that wanted to be added
+ *
+ * Return: ptr to the new added element
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
-	{
-		return NULL;
-	}
+		return (NULL);
 
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
-	return *head;
+	return (*head);
 }
