323355c0f6166c636de1300bbb41dc64cb930ed8
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-07-31 21:23:22 +0300

    task 1. List length  (automated command)

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,22 @@
 #include "lists.h"
+/**
+ * listint_len - function that returns the number of elements in a linked listint_t list.
+ *
+ * @parameters:
+ * @h: ptr to the linked list
+ *
+ * Return: size of the linked list
+ */
 size_t listint_len(const listint_t *h)
 {
 	int len = 0;
+
 	listint_t *curr = (listint_t *)h;
+
 	while (curr != NULL)
 	{
 		len++;
 		curr = curr->next;
 	}
-	return len;
+	return (len);
 }
