7943e6e14c64bd5af579a55b08156ce0a394c630
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-01 13:45:07 +0300

    added betty documentation for task 8. Sum list (automated command)

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
+/**
+ * sum_listint - a  function that returns the sum of all the data (n)
+ * of a listint_t linked list.
+ *
+ * @parameters:
+ * @head: pointer to the first node in the linked list
+ *
+ * Return: the sum of all the data (n) of a listint_t linked list.
+ */
 int sum_listint(listint_t *head)
 {
 	int total_sum = 0;
 	listint_t *curr;
 
 	if (head == NULL)
-		return 0;
+		return (0);
 
 	curr = head;
 
@@ -16,5 +25,5 @@ int sum_listint(listint_t *head)
 		curr = curr->next;
 	}
 
-	return total_sum;
+	return (total_sum);
 }
