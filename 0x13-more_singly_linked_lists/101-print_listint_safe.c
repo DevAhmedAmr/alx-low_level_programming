2447b23022904b876aaadb03e1b5790ae514c19f
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 01:09:49 +0300

    task 12. Print (safe version)  (automated command)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,33 +5,37 @@ int hasAloop(listint_t *head);
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-
 	listint_t *curr = (listint_t *)head;
 
-	listint_t *loop_Check = (listint_t *)head;
-
-	hasAloop(loop_Check);
+	if (hasAloop(curr) == 1)
+	{
+		printf("-> [%p] %i\n", (void *)curr, curr->n);
+		return 0;
+	}
 
 	while (curr != NULL)
 	{
 		printf("[%p] %i\n", (void *)curr, curr->n);
 		count++;
-
 		curr = curr->next;
 	}
+
 	return count;
 }
+
 int hasAloop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
+
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 
 		if (fast == slow)
-			exit(98);
+			return 1;
 	}
-	return 1;
+
+	return 0;
 }
