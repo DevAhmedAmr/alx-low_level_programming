f75d5a04da880ca17cce5d99b6d6f71ff30d6009
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-01 22:30:23 +0300

    task 10. Delete at index  (automated command)

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,4 +1,15 @@
 #include "lists.h"
+/**
+ * delete_nodeint_at_index - function that deletes the node at index
+ *  of a listint_t linked list.
+ *
+ * @param
+ *
+ * @head: pointer to a pointer for the first node in the linked list
+ * @index: index of the element to be removed
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int idx = 0;
@@ -7,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	curr = *head;
 
 	if (head == NULL)
-		return -1;
+		return (-1);
 
 	while (curr != NULL)
 	{
@@ -24,16 +35,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			curr = next;
 
 			if (index >= 1)
-			{
 				before->next = curr;
-			}
 
 			else
-			{
 				*head = curr;
-			}
 
-			return 1;
+			return (1);
 		}
 		else if (idx < index)
 		{
@@ -41,5 +48,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			idx++;
 		}
 	}
-	return -1;
+	return (-1);
 }
