b5aab73896df821ff9ee9412e0045d0665503a66
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-01 19:57:05 +0300

    test (automated command)

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,58 +1,41 @@
 #include "lists.h"
-
-/**
- * delete_nodeint_at_index - deletes a node at an index
- * @head: pointer to the head of the list
- * @index: index of the node to be added
- *
- * Return: the address of the node
- */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0, list_len = listint_len(*head);
+	unsigned int idx = 0;
+	listint_t *curr;
 
-	if ((index > list_len) || (list_len == 0))
-		return (-1);
-	while (head != NULL)
+	curr = *head;
+
+	if (head == NULL)
+		return -1;
+
+	while (curr != NULL)
 	{
-		if (i == index)
+		listint_t *before;
+
+		if (idx + 1 == index)
+			before = curr;
+
+		if (idx == index)
 		{
-			old_node = *head;
-			if (i == 0)
-			{
-				*head = old_node->next;
-				free(old_node);
-				return (1);
-			}
-			previous_node->next = old_node->next;
-			free(old_node);
-			return (1);
+			listint_t *next = curr->next;
+
+			free(curr);
+			curr = next;
+
+			if (index > 1)
+				before->next = curr;
+
+			else
+				*head = curr;
+
+			return 1;
 		}
-		else if ((i + 1) == index)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
+		else if (idx < index)
+		{
+			curr = curr->next;
+			idx++;
+		}
 	}
-	return (-1);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+	return -1;
 }
