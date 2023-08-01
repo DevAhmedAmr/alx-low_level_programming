33de59c66b5a51c1645e746cca883a5e83b31015
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-01 19:19:17 +0300

    task 10. Delete at index  (automated command)

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int idx = 0;
+	listint_t *curr;
+	curr = *head;
+
+	while (curr != NULL)
+	{
+		listint_t *before;
+
+		if (idx + 1 == index)
+			before = curr;
+
+		if (idx == index)
+		{
+			listint_t *next = curr->next;
+
+			free(curr);
+			curr = next;
+
+			if (index > 1)
+			{
+				before->next = curr;
+			}
+			else
+			{
+				*head = curr;
+				// printf("fff");
+			}
+
+			return 1;
+		}
+		else if (idx < index)
+		{
+			curr = curr->next;
+			idx++;
+		}
+	}
+	return -1;
+}
