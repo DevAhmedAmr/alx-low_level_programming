5666fc208f3687442f3b5794d0f3b5e25421ff6d
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-01 13:16:51 +0300

    betty documentation for task .7 (automated command)

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index -  function that returns
+ * the nth node of a listint_t linked list.
+ *
+ * @parameters:
+ * @head: a pointer to the node
+ * @index: the (n)th node of a listint_t linked list to be return
+ *
+ * Return: the (n)th node of a listint_t linked list.
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned idx = 0;
+
 	listint_t *curr;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	curr = head;
 
@@ -16,13 +27,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			idx++;
 			curr = curr->next;
 		}
-		else if (index == idx)
 
+		else if (index == idx)
 			return curr;
 
 		else
-
-			return NULL;
+			return (NULL);
 	}
-	return NULL;
+	return (NULL);
 }
