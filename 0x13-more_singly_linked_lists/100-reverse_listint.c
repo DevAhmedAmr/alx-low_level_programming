162bfba74543269db0a98997749b17ba120ba731
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 00:19:47 +0300

    11. Reverse list  (automated command)

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,4 +1,12 @@
 #include "lists.h"
+/**
+ * reverse_listint - function that reverses a listint_t linked list.
+ *
+ * @parm
+ * @head: a ptr to the first node in a linked list
+ *
+ * Return: a ptr to the first element
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *before = NULL;
@@ -11,5 +19,5 @@ listint_t *reverse_listint(listint_t **head)
 		(*head)->next = before;
 		before = *head;
 	}
-	return *head;
+	return (*head);
 }
