7ed58772fe8a028f3e3cfb207eee1cef0a8e767c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 19:02:05 +0300

    14. Find the loop (automated command)

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *curr = head;
+
+	while (curr != NULL)
+	{
+		listint_t *next = curr->next;
+
+		if (curr > next)
+		{
+			curr = next;
+		}
+		else
+			return curr;
+	}
+	return NULL;
+}
