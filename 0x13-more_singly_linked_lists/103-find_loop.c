00365a563e416b62abef79280bb000d8145e15b2
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 20:43:25 +0300

    (nil) (automated command)

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -17,11 +17,17 @@ listint_t *find_listint_loop(listint_t *head)
 
 		if (curr > next)
 		{
+			if (curr != NULL && next != NULL)
+			{
+				printf("-------------- next %lu %i, current %lu %i\n", (unsigned long int)next, next->n, (unsigned long int)curr, curr->n);
+			}
 
 			curr = next;
 		}
 		else
 		{
+			printf(">>> next %lu %i, current %lu %i\n", (unsigned long int)next, next->n, (unsigned long int)curr, curr->n);
+
 			return next;
 		}
 	}
