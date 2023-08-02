b24cd76334e3373158a4f779deb05d390e7b28b0
Ahmed Amr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 14:07:11 +0300

    task 13. Free (safe version) with the documentation (automated command)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,22 +1,37 @@
 #include "lists.h"
+/**
+ * free_listint_safe -  function that frees a listint_t list.
+ * This function can free lists with a loop
+ *
+ * @param:
+ * @h: pointer to the address of the head node of a linked list
+ *
+ * Return: the size of the list that was free’d
+ */
 size_t free_listint_safe(listint_t **h)
 {
+	size_t list_len = 0;
+
 	listint_t *curr, *next;
+
 	curr = *h;
+
 	while (curr != NULL)
 	{
-		next = *h;
+		next = curr->next;
 		if (curr > next)
 		{
 			free(curr);
 			curr = next;
+			list_len++;
 		}
 		else
 		{
 			free(curr);
+			list_len++;
 			break;
 		}
 	}
-	curr = NULL;
-	return 0;
+	*h = NULL;
+	return list_len;
 }
