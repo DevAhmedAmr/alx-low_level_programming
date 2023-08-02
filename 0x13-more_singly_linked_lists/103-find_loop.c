3b3431c92abc8ea468a6d423afcd37ce7ecd9ed9
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-02 22:43:51 +0300

    14. Find the loop (automated command)

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,35 +1,35 @@
 #include "lists.h"
 
+/*https://www.youtube.com/watch?v=95ZfuoSAUPI*/
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *curr;
-
-	curr = head;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
 	if (head == NULL)
 	{
 		return NULL;
 	}
 
-	while (curr != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		listint_t *next = curr->next;
+		slow = slow->next;
+		fast = fast->next->next;
 
-		if (curr > next)
+		if (fast == slow)
 		{
-			if (curr != NULL && next != NULL)
+			slow = head;
+
+			while (slow != fast)
 			{
-				printf("-------------- next %lu %i, current %lu %i\n", (unsigned long int)next, next->n, (unsigned long int)curr, curr->n);
+				slow = slow->next;
+				fast = fast->next;
+				if (fast == slow)
+
+					return (slow);
 			}
-
-			curr = next;
-		}
-		else
-		{
-			printf(">>> next %lu %i, current %lu %i\n", (unsigned long int)next, next->n, (unsigned long int)curr, curr->n);
-
-			return next;
 		}
 	}
-	return (NULL);
+	return NULL;
 }
