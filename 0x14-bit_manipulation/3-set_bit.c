5a49c9cde9e45a652577e848beba7f19a6e30877
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-04 00:27:49 +0300

    task 3. 11 (automated command)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
 	unsigned int mask = 1 << index;
-
-	*n |= mask;
 
 	if (index > 64)
 		return -1;
+
+	*n |= mask;
 
 	return 1;
 }
