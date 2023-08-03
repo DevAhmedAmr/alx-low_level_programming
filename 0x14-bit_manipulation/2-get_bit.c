1db9cb2dfe23170afdde3cddd27fc429724d088c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-04 00:06:29 +0300

    2. 10 (automated command)

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,16 +1,28 @@
 #include "main.h"
+/**
+ * get_bit -  function that returns the value of a bit
+ * at a given index.
+ *
+ * @param
+ * @index: to be returned
+ * @n: number that want to return index at
+ *
+ * Return:  the value of a bit
+ * at a given index.
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int count = 0;
+
 	while (index < 64)
 	{
 		if (count == index)
 		{
-			return n & 1;
+			return (n & 1);
 		}
 		n >>= 1;
 
 		count++;
 	}
-	return -1;
+	return (-1);
 }
