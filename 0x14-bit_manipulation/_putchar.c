62633a5ad7ab8d6ea63d5360f466a3400ce73fa1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-03 23:04:01 +0300

    added my _putchar (automated command)

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * _putchar  - function take a chars as and input and print them
+ *
+ * @c: prameter char
+ *
+ * Description: print chars
+ *
+ * Return: 0 succsess , else failed
+ */
+int _putchar(char c)
+{
+	write(1, &c, sizeof(c));
+	return (0);
+}
