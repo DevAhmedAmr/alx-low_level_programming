bf50ea8a04300d2e5eb7f924d48089eb11a6c190
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-04 00:29:39 +0300

    task 3. 11 (automated command)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,22 @@
 #include "main.h"
-
+/**
+ * set_bit - function that sets the value of a bit to 1
+ * at a given index.
+ *
+ * @param:
+ * @n: number to be returned form
+ * @index: index to be returned
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask = 1 << index;
 
 	if (index > 64)
-		return -1;
+		return (-1);
 
 	*n |= mask;
 
-	return 1;
+	return (1);
 }
