c5747a7280b757274275ee6ee6eda3d15fadf52a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-04 00:47:58 +0300

    4. 100  (automated command)

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,22 @@
 #include "main.h"
+/**
+ * clear_bit - function that clear a bit in a given index
+ *
+ * @param
+ * @n: number to be cleared
+ * @index: index to be cleared
+ *
+ * Return: 1 if success , 0 on failure
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int bitmask = 1UL << index;
+
 	if (index > 64)
 	{
-		return -1;
+		return (-1);
 	}
 
 	*n &= ~bitmask;
-	return 1;
+	return (1);
 }
