c18239c6adf06673c68ad86879ecbd447618b236
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 02:51:45 +0300

    task 1 (automated command)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,17 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int fd, n;
+	int fd, n, c_value;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
 
 	if (filename == NULL)
 	{
-		(close(fd) == -1);
+
+		c_value = close(fd) == -1;
+
+		if (c_value == -1)
+			return c_value;
 
 		return -1;
 	}
