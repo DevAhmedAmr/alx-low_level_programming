17bfd9192989682cc3cafc792a6c35256b56d10f
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 02:55:55 +0300

    task 1 (automated command)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <string.h>
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, n;
@@ -8,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return -1;
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return -1;
@@ -19,7 +23,10 @@ int create_file(const char *filename, char *text_content)
 	n = write(fd, text_content, strlen(text_content));
 
 	if (n == -1)
+	{
+		close(fd);
 		return -1;
+	}
 
 	if (close(fd) == -1)
 		return -1;
