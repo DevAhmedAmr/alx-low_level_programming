bfc73656730e49053ad028aefd0511005b5b307a
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 02:49:40 +0300

    task 1 (automated command)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,19 @@ int create_file(const char *filename, char *text_content)
 {
 
 	int fd, n;
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
+
+	if (filename == NULL)
+	{
+		(close(fd) == -1);
+
+		return -1;
+	}
+
+	if (text_content == NULL)
+
+		text_content = "";
 
 	if (fd == -1)
 		return -1;
@@ -15,6 +27,9 @@ int create_file(const char *filename, char *text_content)
 	if (n == -1)
 		return -1;
 
-	close(fd);
+	if (close(fd) == -1)
+	{
+		return -1;
+	}
 	return 1;
 }
