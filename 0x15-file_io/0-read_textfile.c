e81e38226b7a2bebfb2c1981e1f49494dd916298
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 01:41:39 +0300

    some optmization is done (automated command)

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer = malloc(sizeof(char) * letters + 1);
 
 	if (buffer == NULL)
-		return NULL;
+		return 0;
 
 	if (filename == NULL)
 		return 0;
