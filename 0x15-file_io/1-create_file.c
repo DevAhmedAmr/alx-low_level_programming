bd8df0ce96c71f935270c8244daca219131acb70
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 03:07:02 +0300

    task 1 (automated command)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,24 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ * create_file - fuction that creates a file with an giving text
+ *  and name for the file
+ *
+ * @param
+ * @text_content: text contented to be saved on a file
+ * @filename: name of the file to be created
+ *
+ * Return:1 on success, -1 on failure
+ * (file can not be created, file can not be written, write “fails”, etc…)
+ *
+ */
 int create_file(const char *filename, char *text_content)
 {
 
-	int fd, n;
+	int fd, write_status;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
@@ -15,15 +26,14 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	if (fd == -1)
-		return -1;
+		return (-1);
 
-	n = write(fd, text_content, strlen(text_content));
+	write_status = write(fd, text_content, strlen(text_content));
 
-	if (n == -1)
-		return -1;
+	if (write_status == -1)
+		return (-1);
 
-	if (close(fd) == -1)
-		return -1;
+	(close(fd));
 
-	return 1;
+	return (1);
 }
