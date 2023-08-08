7f996f392cf27f0cb114501e6d136c98df1943db
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 05:01:58 +0300

    task 3. cp  (automated command)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
