ee2c1cbce00dbe8d44cacb402d33dc269a11cc92
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-08-08 04:59:20 +0300

    task 3. cp  (automated command)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	if (file_to == 1)
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from);
