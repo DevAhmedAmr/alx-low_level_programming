0b4c24ad8acb3f375aa748d79e3408ee12bd086c
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 19:39:10 +0300

    betty is added  (A.C)

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,16 @@
 #include "lists.h"
+dlistint_t *add_head(dlistint_t **head, int n);
+/**
+ * add_dnodeint_end - a  function that adds a new node at
+ * the end of a dlistint_t list.
+ *
+ * @param
+ * @head: the head of linked list that wanted an element to be added in
+ * @n: element to be added in the linked list
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *curr = *head;
@@ -6,21 +18,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last_node;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	if (*head == NULL)
-	{
-		curr = malloc(sizeof(dlistint_t) * 1);
-
-		if (curr == NULL)
-			return NULL;
-
-		curr->n = n;
-		curr->next = NULL;
-		curr->prev = NULL;
-		*head = curr;
-		return curr;
-	}
+		return (add_head(head, n));
 
 	while (curr != NULL)
 	{
@@ -32,13 +33,35 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_Node = malloc(sizeof(dlistint_t) * 1);
 
 	if (new_Node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_Node->n = n;
 	new_Node->next = NULL;
 	new_Node->prev = last_node;
-
 	last_node->next = new_Node;
+	return (new_Node);
+}
 
-	return new_Node;
+/**
+ * add_head - a  function that adds a new node
+ * as the new head for a linked list
+ *
+ * @param
+ * @head: a null pointer to an empty list
+ * @n: element to be added in the linked list as the new head
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_head(dlistint_t **head, int n)
+{
+	dlistint_t *curr = malloc(sizeof(dlistint_t) * 1);
+
+	if (curr == NULL)
+		return (NULL);
+
+	curr->n = n;
+	curr->next = NULL;
+	curr->prev = NULL;
+	*head = curr;
+	return (curr);
 }
