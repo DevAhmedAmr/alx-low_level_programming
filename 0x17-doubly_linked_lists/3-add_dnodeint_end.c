d8fda6cbdd2b9c4b613e43c39e6c059f4c5746b4
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 18:43:46 +0300

    task 3. Add node at the end  (A.C)

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *curr = *head;
+	dlistint_t *new_Node;
+	dlistint_t *last_node;
+
+	if (*head == NULL)
+	{
+		curr = malloc(sizeof(dlistint_t) * 1);
+		curr->n = n;
+		curr->next = NULL;
+		*head = curr;
+
+		return curr;
+	}
+
+	while (curr != NULL)
+	{
+		if (curr->next == NULL)
+			last_node = curr;
+		curr = curr->next;
+	}
+
+	new_Node = last_node->next;
+
+	new_Node = malloc(sizeof(dlistint_t) * 1);
+	new_Node->n = n;
+	new_Node->next = NULL;
+	new_Node->prev = curr;
+
+	last_node->next = new_Node;
+
+	return new_Node;
+}
