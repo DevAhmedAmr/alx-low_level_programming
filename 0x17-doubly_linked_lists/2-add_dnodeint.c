3db26646dea23e361b52636a3bc4aec1e0c0611b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 20:17:55 +0300

    2. Add node  (A.C)

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *curr = *head;
 	dlistint_t *new_Node;
 
+	if (head == NULL)
+	{
+		return NULL;
+	}
+
 	if (*head == NULL)
 		return (add_head(head, n));
 
