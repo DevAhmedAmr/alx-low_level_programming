f1ea6667bb7359c8bc37477091184bf9c90418bf
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 21:29:35 +0300

    task 4. Free list  (A.C)

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * free_dlistint -  function that frees a dlistint_t list.
+ * @head: a pointer to the first node to be deleted
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *curr = head;
@@ -6,6 +11,7 @@ void free_dlistint(dlistint_t *head)
 	while (curr)
 	{
 		dlistint_t *next = curr->next;
+
 		free(curr);
 		curr = next;
 	}
