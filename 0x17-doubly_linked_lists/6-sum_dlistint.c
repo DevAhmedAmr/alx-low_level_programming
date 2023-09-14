8b12464846953fc13c0cc9443198265e1e77a496
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 22:32:32 +0300

    6. Sum list gitp (A.C)

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,23 @@
 #include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of all
+ * the data (n) of a dlistint_t linked list.
+ * @param
+ * @head: a node to a linked list
+ *
+ * Return: the sum of all the data (n) of a dlistint_t linked list.
+ *
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 	dlistint_t *curr = head;
+
 	while (curr)
 	{
 		sum += curr->n;
 
 		curr = curr->next;
 	}
-	return sum;
+	return (sum);
 }
