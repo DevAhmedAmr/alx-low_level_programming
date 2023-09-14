2e25f1243237647824fc6636d1039047021ef763
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 17:13:23 +0300

    0. Print list  (A.C)

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_dlistint - function that prints a element of n of a linked list
+ *
+ * @param
+ * @h: a node to the head of the linked list
+ *
+ * Return: the length of the linked list
+ *
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *curr = h;
 	size_t n = 0;
+
 	while (curr != NULL)
 	{
 		printf("%i\n", curr->n);
 		curr = curr->next;
 		n++;
 	}
-	return n;
+	return (n);
 }
