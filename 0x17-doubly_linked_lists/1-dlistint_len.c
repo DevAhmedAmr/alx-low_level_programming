b5386faf87f601399472fa084cd48db5ca6643e2
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-14 17:19:25 +0300

    task 1. List length  (A.C)

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - function that returns the number of elements in a linked dlistint_t list.
+ *
+ * @param
+ * @h: a node to the head of the linked list
+ *
+ * Return: the length of the linked list
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *curr = h;
+	size_t n = 0;
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+		n++;
+	}
+	return (n);
+}
