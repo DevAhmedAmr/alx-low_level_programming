a23a91388d17dec01181e742f4ccce6f3388fb67
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-15 05:14:26 +0300

    first commit (A.C)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	if (*head == NULL)
-		return 1; /* Return -1 for an empty list.*/
+		return -1; /* Return -1 for an empty list.*/
 
 	/* Handling the case of deleting the first node (index = 0).*/
 	if (index == 0)
@@ -35,5 +35,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		i++;
 	}
 
-	return 1; /* Index is out of bounds.*/
+	return -1; /* Index is out of bounds.*/
 }
