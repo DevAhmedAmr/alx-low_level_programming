670afd1bcb28cb66059d30da7af687826ecd4276
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-18 22:47:54 +0300

    task 0. A library is not a luxury but one of the necessities of life  (A.C)

--- 0x18-dynamic_libraries/_atoi.c
+++ 0x18-dynamic_libraries/_atoi.c
@@ -9,7 +9,7 @@
 
 int _atoi(char *s)
 {
-	int operator= 1;
+	int operator = 1;
 
 	unsigned int num = 0;
 
@@ -25,10 +25,10 @@ int _atoi(char *s)
 
 		if (ascii_code == 45)
 
-			operator*= - 1;
+			operator *= -1;
 		else if (ascii_code == 43)
 
-			operator*= 1;
+			operator *= 1;
 		if ((num > 0 && ascii_code < 47) || (num > 0 && ascii_code > 58))
 			break;
 		else if ((ascii_code < 47) || ascii_code > 58)
