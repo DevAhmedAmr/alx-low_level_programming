3dbfd28b3fc005c26daebac60da2f492624b3b2b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-18 21:26:44 +0300

    task 9 - fixed delete at index 0 bug (A.C)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
+/**
+ * delete_dnodeint_at_index - function that deletes the node
+ * at index index of a dlistint_t linked list.
+ * @head: pointer to a linked listed to be deleted
+ * @index: the index to be deleted
+ *
+ * Return: Returns: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *curr, *tmp;
 	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
-		return -1;
+		return (-1);
 	curr = *head;
 
 	if (index == 0)
@@ -17,9 +25,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 		*head = curr->next;
 		free(tmp);
-		return 1;
+		return (1);
 	}
-
 	for (; curr; curr = curr->next, i++)
 	{
 		if ((curr->next == NULL) && (i == index))
@@ -27,9 +34,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			tmp = curr;
 			curr->prev->next = NULL;
 			free(tmp);
-			return 1;
+			return (1);
 		}
-
 		if (i == index)
 		{
 			tmp = curr;
@@ -40,5 +46,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (1);
 		}
 	}
-	return -1;
+	return (-1);
 }
