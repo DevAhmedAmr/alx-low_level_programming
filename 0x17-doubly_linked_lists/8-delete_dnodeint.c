3d7557b1bc1b5f3a8d456609caa140a58c850aa6
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-09-18 21:17:43 +0300

    first commit (A.C)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,11 +7,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (head == NULL || *head == NULL)
 		return -1;
 	curr = *head;
-
 	if (index == 0)
 	{
 		tmp = curr;
-		curr->prev->next = NULL;
+		curr->prev = NULL;
 		*head = curr->next;
 		free(tmp);
 		return 1;
