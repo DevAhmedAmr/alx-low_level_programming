a5c10f2d297fd0ea15d4b7b9d567b003f272d91d
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-06 02:24:32 +0300

    2. key -> index (A.C)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -48,11 +48,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			curr = malloc(sizeof(hash_node_t));
-			curr->key = strdup(key);
-			curr->value = strdup(value);
-			curr->next = NULL;
-			free(new_node);
+			ht->array[index] = new_node;
 		}
 	}
 
