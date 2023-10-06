78569dbd9f0700b69197aaea8345d3eb907bd41b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-06 18:38:32 +0300

    4. >>> ht['betty'] (A.C)

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	size_t index = key_index((const unsigned char *)key, 1024);
+	size_t index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *curr = ht->array[index];
 
 	while (curr != NULL)
 	{
-		if (strcmp(curr->key, (char *)key) == 0)
+		if (strcmp(curr->key, key) == 0)
 		{
 			return curr->value;
 		}
