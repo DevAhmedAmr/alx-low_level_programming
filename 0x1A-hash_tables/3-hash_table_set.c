9254b1d0be1883c6937e6feea5369af65c6b6584
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-10-06 16:41:34 +0300

    first commit (A.C)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,36 +24,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = malloc(sizeof(hash_node_t));
-		if (ht->array[index] == NULL)
-			return (0);
 		ht->array[index]->key = strdup((char *)key);
-		if (ht->array[index]->key == NULL)
-			return (0);
 		ht->array[index]->value = strdup((char *)value);
-		if (ht->array[index]->value == NULL)
-		{
-			free(ht->array[index]->key);
-			free(ht->array[index]);
-			return (0);
-		}
 		ht->array[index]->next = NULL;
 	}
 	else
 	{
 		hash_node_t *curr = ht->array[index];
+		/*hash_node_t *prev = ht->array[index];*/
 
 		while (curr != NULL && strcmp(curr->key, key) != 0)
 		{
+
+			/*prev = curr;*/
 			curr = curr->next;
 		}
 
 		if (curr != NULL && strcmp(curr->key, key) == 0)
 		{
-			free(curr->value);
-			curr->value = strdup(value);
+			// free(curr->value);
+			// curr->value = strdup(value);
 
-			if (curr->value == NULL)
-				return 0;
+			// if (curr->value == NULL)
+			return 0;
 		}
 		else
 			insert_stack(ht, (char *)key, (char *)value);
