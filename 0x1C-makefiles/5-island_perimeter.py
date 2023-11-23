228bf2db90d506666602eec6896e145e67bebdd1
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-23 04:15:11 +0200

    5. Island Perimeter (automated command)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,36 +6,39 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in island grid"""
     perimeter: int = 0
     first_one = 0
-    last_one = [0, 0]
-    number_of_ones = 0
     for i in range(len(grid)):
-        number_of_ones = 0
         for j in range(len(grid[i])):
+            print(look_up(grid, i, j))
             if grid[i][j] == 1:
-                if first_one == 0:
-                    if grid[i][j] < len(grid) - 1 and grid[i][j + 1] == 1:
-                        perimeter += 2
-                    else:
-                        perimeter += 3
-
-                    first_one = 1
-                else:
-                    if grid[i][j] < len(grid[i]) - 1:
-                        perimeter += 2
-                number_of_ones += 1
-
-    if number_of_ones == 1:
-        return perimeter + 1
+                perimeter += look_up(grid, i, j)
 
     return perimeter
+
+
+def look_up(grid, i, j):
+    sides = 0
+
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    if i > 0 and grid[i - 1][j] == 0:
+        sides += 1
+    if i < rows - 1 and grid[i + 1][j] == 0:
+        sides += 1
+    if j > 0 and grid[i][j - 1] == 0:
+        sides += 1
+    if j < cols - 1 and grid[i][j + 1] == 0:
+        sides += 1
+
+    return sides
 
 
 if __name__ == "__main__":
     grid = [
         [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0],
         [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0],
     ]
     print(island_perimeter(grid))
