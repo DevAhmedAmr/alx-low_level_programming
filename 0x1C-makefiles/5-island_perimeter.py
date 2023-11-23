056c27c45f73ae57ddac998c60586a441a8c6585
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-23 03:52:49 +0200

    5. Island Perimeter (automated command)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,10 @@ def island_perimeter(grid):
     """returns the perimeter of the island described in island grid"""
     perimeter: int = 0
     first_one = 0
+    last_one = [0, 0]
+    number_of_ones = 0
     for i in range(len(grid)):
+        number_of_ones = 0
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 if first_one == 0:
@@ -19,13 +22,17 @@ def island_perimeter(grid):
                 else:
                     if grid[i][j] < len(grid[i]) - 1:
                         perimeter += 2
+                number_of_ones += 1
 
-    return perimeter + 1
+    if number_of_ones == 1:
+        return perimeter + 1
+
+    return perimeter
 
 
 if __name__ == "__main__":
     grid = [
-        [0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0],
         [0, 1, 1, 0, 0],
         [0, 1, 0, 0, 0, 0],
         [0, 1, 1, 1, 0, 0],
