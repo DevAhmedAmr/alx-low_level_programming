3e426f5e7da1e04ee0a13782988ca4ec7b488e7b
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2023-11-23 04:16:21 +0200

    5. Island Perimeter (automated command)

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,30 +3,33 @@
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in island grid"""
-    perimeter: int = 0
-    first_one = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    """Returns the perimeter of the island described in the grid"""
+    perimeter = 0
+
+    if not grid or not grid[0]:  # Check for empty grid
+        return perimeter
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += look_up(grid, i, j)
+                perimeter += look_up(grid, i, j, rows, cols)
 
     return perimeter
 
 
-def look_up(grid, i, j):
+def look_up(grid, i, j, rows, cols):
     sides = 0
 
-    rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
-
-    if i > 0 and grid[i - 1][j] == 0:
+    if i == 0 or grid[i - 1][j] == 0:  # Top side
         sides += 1
-    if i < rows - 1 and grid[i + 1][j] == 0:
+    if i == rows - 1 or grid[i + 1][j] == 0:  # Bottom side
         sides += 1
-    if j > 0 and grid[i][j - 1] == 0:
+    if j == 0 or grid[i][j - 1] == 0:  # Left side
         sides += 1
-    if j < cols - 1 and grid[i][j + 1] == 0:
+    if j == cols - 1 or grid[i][j + 1] == 0:  # Right side
         sides += 1
 
     return sides
