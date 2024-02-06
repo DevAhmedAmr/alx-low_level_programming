fa195a752973f0b3fd8081484d925e2c437c3890
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-02-06 23:23:09 +0200

    1. Binary search (automated command)

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,6 +2,7 @@
 /**
  * linear_search - function that searches for a value in an array of
  * integers using the Linear search algorithm
+ *
  * @params
  * @array: array to be searched
  * @size: size of the array
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+int binary_search_helper(int *array, size_t start, size_t end, int value);
+int print_arr(int *arr, size_t start, size_t size);
+int binary_search(int *array, size_t size, int value)
+{
+
+	return binary_search_helper(array, 0, size - 1, value);
+}
+int binary_search_helper(int *array, size_t start, size_t end, int value)
+{
+	size_t mid = (start + end) / 2;
+
+	print_arr(array, start, end);
+
+	if (start == end || start > end)
+		return -1;
+
+	if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else if (array[mid] < value)
+		/*[1,2,3,4,5]*/
+		return binary_search_helper(array, mid + 1, end, value);
+	else if (array[mid] > value)
+		return binary_search_helper(array, start, mid - 1, value);
+
+	return -1;
+}
+
+int print_arr(int *arr, size_t start, size_t size)
+{
+	size_t i;
+
+	if (arr == NULL || size == 0)
+	{
+		return -1;
+	}
+
+	printf("Searching in array:");
+
+	for (i = start; i <= size; i++)
+	{
+
+		printf(" %i", arr[i]);
+
+		if (i != size)
+			printf(",");
+	}
+	printf("\n");
+	return 0;
+}
