b2f0a071eca678fd0617d487ed89ba55ea1461c2
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-02-07 00:18:10 +0200

    1. Binary search (automated command)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,19 +13,18 @@ int binary_search_helper(int *array, size_t start, size_t end, int value)
 	print_arr(array, start, end);
 
 	if (start > end || array == NULL)
-		return -1;
+		return (-1);
 
 	if (array[mid] == value)
-	{
 		return (mid);
-	}
-	else if (array[mid] < value)
-		/*[1,2,3,4,5]*/
-		return binary_search_helper(array, mid + 1, end, value);
-	else if (array[mid] > value)
-		return binary_search_helper(array, start, mid - 1, value);
 
-	return -1;
+	else if (array[mid] < value)
+		return (binary_search_helper(array, mid + 1, end, value));
+
+	else if (array[mid] > value)
+		return (binary_search_helper(array, start, mid - 1, value));
+
+	return (-1);
 }
 
 int print_arr(int *arr, size_t start, size_t size)
