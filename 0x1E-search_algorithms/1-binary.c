6ebd7675abebfb4d73084520d7ec2bfa255fbeaa
DevAhmedAmr <ahmedamr989dyhdhdhd@gmail.com>
2024-02-07 00:34:16 +0200

    1. Binary search (automated command)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,6 +6,18 @@ int binary_search(int *array, size_t size, int value)
 
 	return binary_search_helper(array, 0, size - 1, value);
 }
+/**
+ * binary_search_helper - a function that searches for a value in
+ * a sorted array of integers using the Binary search algorithm
+ *
+ * @params
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be found
+ *
+ * Return: If value is not present in array or if array is NULL, return -1
+ * else return the index of the value
+ */
 int binary_search_helper(int *array, size_t start, size_t end, int value)
 {
 	size_t mid = (start + end) / 2;
@@ -26,24 +38,34 @@ int binary_search_helper(int *array, size_t start, size_t end, int value)
 
 	return (-1);
 }
+/**
+ * print_arr - a function that print the array
 
-int print_arr(int *arr, size_t start, size_t size)
+ * * @params
+ * @array: array to be printed
+ * @start: start of the array
+ * @end:
+ *
+ * Return: If value is not present in array or if array is NULL, return -1
+ * else return the index of the value
+ */
+int print_arr(int *arr, size_t start, size_t end)
 {
 	size_t i;
 
-	if (arr == NULL || size == 0 || start > size)
+	if (arr == NULL || end == 0 || start > end)
 	{
 		return -1;
 	}
 
 	printf("Searching in array:");
 
-	for (i = start; i <= size; i++)
+	for (i = start; i <= end; i++)
 	{
 
 		printf(" %i", arr[i]);
 
-		if (i != size)
+		if (i != end)
 			printf(",");
 	}
 	printf("\n");
